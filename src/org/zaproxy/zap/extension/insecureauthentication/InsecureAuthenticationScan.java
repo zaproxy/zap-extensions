26fd319777cc8e571ac204dfcf7fe95e714f5269
colm.p.oflaherty@gmail.com <colm.p.oflaherty@gmail.com@1e26c7e7-97f5-7f45-da66-271314645f9b>
2012-07-30 14:23:55 +0000

    Tidy up. No functional changes.

--- src/org/zaproxy/zap/extension/insecureauthentication/InsecureAuthenticationScan.java
+++ src/org/zaproxy/zap/extension/insecureauthentication/InsecureAuthenticationScan.java
@@ -49,50 +49,50 @@ public class InsecureAuthenticationScan extends PluginPassiveScanner implements 
 	 * the Passive Scanner Thread
 	 */
 	private PassiveScanThread parent = null;
-	    
-    /**
-     * for logging.
-     */
-    private static Logger log = Logger.getLogger(InsecureAuthenticationScan.class);
-    
-    /**
-     * determines if we should output Debug level logging
-     */
-    private boolean debugEnabled = log.isDebugEnabled(); 
 
-    /**
-     * contains the internationalisation (i18n) messages. Must be statically initialised, since messages is accessed before the plugin is initialised (using init)
-     */
-    private ResourceBundle messages = ResourceBundle.getBundle(
-            this.getClass().getPackage().getName() + ".Messages", Constant.getLocale());
-	
+	/**
+	 * for logging.
+	 */
+	private static Logger log = Logger.getLogger(InsecureAuthenticationScan.class);
 
-    /**
-     * gets the internationalised message corresponding to the key
-     * @param key the key to look up the internationalised message
-     * @return the internationalised message corresponding to the key
-     */
-    public String getString(String key) {
-        try {
-            return messages.getString(key);
-        } catch (MissingResourceException e) {
-            return '!' + key + '!';
-        }
-    }
-    
-    /**
-     * gets the internationalised message corresponding to the key, using the parameters supplied
-     * @param key the key to look up the internationalised message
-     * @param params the parameters used to internationalise the message
-     * @return the internationalised message corresponding to the key, using the parameters supplied
-     */
-    public String getString(String key, Object... params  ) {
-        try {
-            return MessageFormat.format(messages.getString(key), params);
-        } catch (MissingResourceException e) {
-            return '!' + key + '!';
-        }
-    }
+	/**
+	 * determines if we should output Debug level logging
+	 */
+	private boolean debugEnabled = log.isDebugEnabled(); 
+
+	/**
+	 * contains the internationalisation (i18n) messages. Must be statically initialised, since messages is accessed before the plugin is initialised (using init)
+	 */
+	private ResourceBundle messages = ResourceBundle.getBundle(
+			this.getClass().getPackage().getName() + ".Messages", Constant.getLocale());
+
+
+	/**
+	 * gets the internationalised message corresponding to the key
+	 * @param key the key to look up the internationalised message
+	 * @return the internationalised message corresponding to the key
+	 */
+	public String getString(String key) {
+		try {
+			return messages.getString(key);
+		} catch (MissingResourceException e) {
+			return '!' + key + '!';
+		}
+	}
+
+	/**
+	 * gets the internationalised message corresponding to the key, using the parameters supplied
+	 * @param key the key to look up the internationalised message
+	 * @param params the parameters used to internationalise the message
+	 * @return the internationalised message corresponding to the key, using the parameters supplied
+	 */
+	public String getString(String key, Object... params  ) {
+		try {
+			return MessageFormat.format(messages.getString(key), params);
+		} catch (MissingResourceException e) {
+			return '!' + key + '!';
+		}
+	}
 
 	@Override
 	public void setParent (PassiveScanThread parent) {
@@ -101,54 +101,53 @@ public class InsecureAuthenticationScan extends PluginPassiveScanner implements 
 
 	@Override
 	public void scanHttpRequestSend(HttpMessage msg, int id) {
-		
+
 		String uri = null, method = null;
 		String extraInfo = null;  //valeu depends on which method is being used.
 		String digestInfo = null;
-		
+
 		//DEBUG only
 		//log.setLevel(org.apache.log4j.Level.DEBUG);
-    	//this.debugEnabled = true;
+		//this.debugEnabled = true;
 
-    	if (msg.getRequestHeader().getSecure()) {
+		if (msg.getRequestHeader().getSecure()) {
 			// If SSL is used then the use of 'weak' authentication methods isn't really an issue
-    		return;
-			}
-    	
-    	//get the URI
-    	try {
-    		uri = msg.getRequestHeader().getURI().getURI().toString();
-    		method = msg.getRequestHeader().getMethod();
-    	}
-    	catch (Exception e) {
-    		log.error("Error getting URI from message ["+msg+"]");
-    		return;
-    	}
-		
+			return;
+		}
+
+		//get the URI
+		try {
+			uri = msg.getRequestHeader().getURI().getURI().toString();
+			method = msg.getRequestHeader().getMethod();
+		}
+		catch (Exception e) {
+			log.error("Error getting URI from message ["+msg+"]");
+			return;
+		}
+
 		Vector<String> headers = msg.getRequestHeader().getHeaders(HttpHeader.AUTHORIZATION);
 		if (headers != null) {					
 			for (Iterator<String> i = headers.iterator(); i.hasNext();) {
 				String authHeaderValue = i.next();
 				String authMechanism = null;
 				if (    ( authHeaderValue.toLowerCase(Locale.ENGLISH).startsWith("basic ")) ||
-						( authHeaderValue.toLowerCase(Locale.ENGLISH).startsWith("digest ")) )
-				{				
+						( authHeaderValue.toLowerCase(Locale.ENGLISH).startsWith("digest ")) ) {				
 					int alertRisk=Alert.RISK_MEDIUM; //Medium by default.. maybe even high.
 					int alertLevel = Alert.WARNING; 
 					String username=null, password=null;
-					
+
 					//gets Basic or Digest.. (trailing spaces trimmed off)
 					authMechanism = authHeaderValue.substring(0, 6).trim();
-					
+
 					//Handle Basic Auth
 					if ( authMechanism.toLowerCase(Locale.ENGLISH).equals ("basic") ) {
 						//Basic authentication... the username and password are merely base64 encoded and 
 						//sent over the wire.. not good!
 						String [] authValues = authHeaderValue.split(" ");  //do NOT convert to lowercase for the split.. will corrupt the base64 data
 						if (authValues.length == 2) { 
-		    				if ( this.debugEnabled ) log.debug(authMechanism +" Authentication Value: "+ authValues[1]);
-		    				//now decode it from base64 into the username and password
-		    				try {
+							if ( this.debugEnabled ) log.debug(authMechanism +" Authentication Value: "+ authValues[1]);
+							//now decode it from base64 into the username and password
+							try {
 								String decoded  = new String (Base64.decode(authValues[1]));
 								if ( this.debugEnabled ) log.debug("Decoded Base64 value: "+ decoded);
 								String [] usernamePassword = decoded.split(":", 2);
@@ -161,77 +160,69 @@ public class InsecureAuthenticationScan extends PluginPassiveScanner implements 
 								}
 								if ( password != null) {
 									alertRisk=Alert.RISK_HIGH;
-									}
-		    					} 
-		    				catch (IOException e) {
-		    						log.error("Invalid Base64 value for "+authMechanism+" Authentication: "+ authValues[1]);
-		    					}
+								}
+							} 
+							catch (IOException e) {
+								log.error("Invalid Base64 value for "+authMechanism+" Authentication: "+ authValues[1]);
 							}
+						}
 						else {
 							//malformed Basic Auth header?? warn, but ignore
 							if ( this.debugEnabled ) log.debug("Malformed "+authMechanism+" Authentication Header: ["+ authHeaderValue + "], "+ authValues.length + " values found");
 							continue; //to the next header
-							}
+						}
 						extraInfo = getString("authenticationcredentialscaptured.alert.basicauth.extrainfo", 
 								method, uri, authMechanism, username, password);
-						}
-					
+					}
+
 					//Handle Digest Auth
 					if ( authMechanism.toLowerCase(Locale.ENGLISH).equals ("digest") ) {
 						alertRisk=Alert.RISK_MEDIUM;  //not as high as for Basic Auth, but worth raising as an issue.
-											
+
 						//Digest authentication... the username is in plaintext, and the password is hashed
 						String [] authValues = authHeaderValue.split(" ", 2);  //do NOT convert to lowercase for the split.. will corrupt the base64 data
 						if (authValues.length == 2) {
-		    				if ( this.debugEnabled ) log.debug(authMechanism +" Authentication Value: "+ authValues[1]);
-		    				//now grab the username from the string	    				
-	    					Pattern pattern = Pattern.compile(".*username=\"([^\"]+)\".*");
-	    					Matcher matcher = pattern.matcher(authValues[1]);
-	    					if (matcher.matches()) {
-	    					    username = matcher.group(1);
-	    					} else {
-	    						//no username in the Digest??
-	    						if ( this.debugEnabled ) log.debug("Malformed "+authMechanism+" Authentication Header: ["+ authHeaderValue + "]. No username was found");
-	    						continue; //to the next header..
-	    						}	    					
-							}
+							if ( this.debugEnabled ) log.debug(authMechanism +" Authentication Value: "+ authValues[1]);
+							//now grab the username from the string	    				
+							Pattern pattern = Pattern.compile(".*username=\"([^\"]+)\".*");
+							Matcher matcher = pattern.matcher(authValues[1]);
+							if (matcher.matches()) {
+								username = matcher.group(1);
+							} else {
+								//no username in the Digest??
+								if ( this.debugEnabled ) log.debug("Malformed "+authMechanism+" Authentication Header: ["+ authHeaderValue + "]. No username was found");
+								continue; //to the next header..
+							}	    					
+						}
 						else {
 							//malformed Digest Auth header?? warn, but ignore
 							if ( this.debugEnabled ) log.debug("Malformed "+authMechanism+" Authentication Header: ["+ authHeaderValue + "], "+ authValues.length + " values found");
 							continue; //to the next header
-							}
-						
+						}
+
 						extraInfo = getString("authenticationcredentialscaptured.alert.digestauth.extrainfo", 
 								method, uri, authMechanism, username, authValues[1]);
 						digestInfo=authValues[1]; //info to output in the logging message.
-						}
-							
-	    			String attack = getString("authenticationcredentialscaptured.alert.attack");
-	    			String vulnname=getString("authenticationcredentialscaptured.name");
-	    			String vulndesc=getString("authenticationcredentialscaptured.desc");
-	    			String vulnsoln=getString("authenticationcredentialscaptured.soln");
-	    			String vulnrefs=getString("authenticationcredentialscaptured.refs");
-	    			
-	    			Alert alert = new Alert(getId(), alertRisk, alertLevel, vulnname);
-	    			alert.setDetail(vulndesc, 
-				    		uri,
-				    		"",  //No specific parameter. It's in the header.
-				    		attack, 
-				    		extraInfo, 
-				    		vulnsoln, 
-				    		vulnrefs, 
-				            msg);
-	    			//raise the alert
-	    			parent.raiseAlert(id, alert);
-	    			
-	    			//and log it
-					String logMessage = getString ("authenticationcredentialscaptured.alert.logmessage", 
-								method, uri, authMechanism, username, (digestInfo!=null)?digestInfo:password);
-					log.info(logMessage);
-					} //basic or digest authorisation
-				} //end of authorization headers		
-			} //end of headers null check
-		} //end of method
+					}
+
+					Alert alert = new Alert(getId(), alertRisk, alertLevel, getString("authenticationcredentialscaptured.name"));
+					alert.setDetail(getString("authenticationcredentialscaptured.desc"), 
+							uri,
+							"",  //No specific parameter. It's in the header.
+							getString("authenticationcredentialscaptured.alert.attack"),
+							extraInfo, 
+							getString("authenticationcredentialscaptured.soln"), 
+							getString("authenticationcredentialscaptured.refs"), 
+							msg);
+					//raise the alert
+					parent.raiseAlert(id, alert);
+
+					//and log it, without internationalising it.
+					log.info("Authentication Credentials were captured. ["+method+"] ["+uri+"] uses insecure authentication mechanism ["+authMechanism+"], revealing username ["+username+"] and password/additional information ["+((digestInfo!=null)?digestInfo:password)+"]");
+				} //basic or digest authorisation
+			} //end of authorization headers		
+		} //end of headers null check
+	} //end of method
 
 	/**
 	 * gets the plugin id
@@ -248,34 +239,34 @@ public class InsecureAuthenticationScan extends PluginPassiveScanner implements 
 	public String getName() {
 		return getString("insecureauthentication.name");
 	}
-	
-    /* (non-Javadoc)
-     * @see com.proofsecure.paros.core.scanner.Test#getDescription()
-     */
-    public String getDescription() {
-    	return getString("insecureauthentication.desc");
-    }
 
-    /* (non-Javadoc)
-     * @see com.proofsecure.paros.core.scanner.Test#getCategory()
-     */
-    public int getCategory() {
-        return Category.INFO_GATHER;  //leaking username or username + password.. therefore information gathering
-    }
+	/* (non-Javadoc)
+	 * @see com.proofsecure.paros.core.scanner.Test#getDescription()
+	 */
+	public String getDescription() {
+		return getString("insecureauthentication.desc");
+	}
 
-    /* (non-Javadoc)
-     * @see com.proofsecure.paros.core.scanner.Test#getSolution()
-     */
-    public String getSolution() {
-    	return getString("insecureauthentication.soln");
-    }
+	/* (non-Javadoc)
+	 * @see com.proofsecure.paros.core.scanner.Test#getCategory()
+	 */
+	public int getCategory() {
+		return Category.INFO_GATHER;  //leaking username or username + password.. therefore information gathering
+	}
 
-    /* (non-Javadoc)
-     * @see com.proofsecure.paros.core.scanner.Test#getReference()
-     */
-    public String getReference() {
-    	return getString("insecureauthentication.refs");  
-    }
+	/* (non-Javadoc)
+	 * @see com.proofsecure.paros.core.scanner.Test#getSolution()
+	 */
+	public String getSolution() {
+		return getString("insecureauthentication.soln");
+	}
+
+	/* (non-Javadoc)
+	 * @see com.proofsecure.paros.core.scanner.Test#getReference()
+	 */
+	public String getReference() {
+		return getString("insecureauthentication.refs");  
+	}
 
 	@Override
 	public void scanHttpResponseReceive(HttpMessage msg, int id, Source source) {
@@ -288,19 +279,19 @@ public class InsecureAuthenticationScan extends PluginPassiveScanner implements 
 			for (String auth : authHeaders) {
 				if (auth.toLowerCase().indexOf("basic") > -1 || auth.toLowerCase().indexOf("digest") > -1) {
 					Alert alert = new Alert(getId(), Alert.RISK_MEDIUM, Alert.WARNING,
-							getString("insecureauthentication.name"));
-					alert.setDetail(getString("insecureauthentication.desc"),
+							getName());
+					alert.setDetail(getDescription(),
 							msg.getRequestHeader().getURI().toString(),
 							"", HttpHeader.WWW_AUTHENTICATE + ": " + auth,
 							"",
-							getString("insecureauthentication.soln"),
-							getString("insecureauthentication.refs"),
+							getSolution(),
+							getReference(),
 							msg);
 					parent.raiseAlert(id, alert);
 				}
 			}
 		}
 	}		
-	
+
 
 }
