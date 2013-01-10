217a041e4a4d342831e1f57a24f0b5036746bd5c
robert.koch@loggia.at <robert.koch@loggia.at@1e26c7e7-97f5-7f45-da66-271314645f9b>
2013-01-10 12:43:57 +0000

    added Server-Sent Events (SSE) extension SSE help files automatically merge into help

---
+++ src/org/zaproxy/zap/extension/sse/ExtensionServerSentEvents.java
@@ -0,0 +1,486 @@
+/*
+ * Zed Attack Proxy (ZAP) and its related class files.
+ * 
+ * ZAP is an HTTP/HTTPS proxy for assessing web application security.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); 
+ * you may not use this file except in compliance with the License. 
+ * You may obtain a copy of the License at 
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0 
+ *   
+ * Unless required by applicable law or agreed to in writing, software 
+ * distributed under the License is distributed on an "AS IS" BASIS, 
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+ * See the License for the specific language governing permissions and 
+ * limitations under the License. 
+ */
+package org.zaproxy.zap.extension.sse;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.Socket;
+import java.nio.charset.Charset;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.log4j.Logger;
+import org.parosproxy.paros.Constant;
+import org.parosproxy.paros.control.Control.Mode;
+import org.parosproxy.paros.db.Database;
+import org.parosproxy.paros.extension.ExtensionAdaptor;
+import org.parosproxy.paros.extension.ExtensionHook;
+import org.parosproxy.paros.extension.ExtensionHookView;
+import org.parosproxy.paros.extension.SessionChangedListener;
+import org.parosproxy.paros.model.Model;
+import org.parosproxy.paros.model.Session;
+import org.parosproxy.paros.network.HttpMessage;
+import org.parosproxy.paros.view.View;
+import org.zaproxy.zap.PersistentConnectionListener;
+import org.zaproxy.zap.ZapGetMethod;
+import org.zaproxy.zap.extension.help.ExtensionHelp;
+import org.zaproxy.zap.extension.httppanel.Message;
+import org.zaproxy.zap.extension.httppanel.component.HttpPanelComponentInterface;
+import org.zaproxy.zap.extension.httppanel.view.HttpPanelDefaultViewSelector;
+import org.zaproxy.zap.extension.httppanel.view.HttpPanelView;
+import org.zaproxy.zap.extension.httppanel.view.hex.HttpPanelHexView;
+import org.zaproxy.zap.extension.sse.db.EventStreamStorage;
+import org.zaproxy.zap.extension.sse.db.TableEventStream;
+import org.zaproxy.zap.extension.sse.ui.EventStreamPanel;
+import org.zaproxy.zap.extension.sse.ui.httppanel.component.EventStreamComponent;
+import org.zaproxy.zap.extension.sse.ui.httppanel.models.ByteEventStreamPanelViewModel;
+import org.zaproxy.zap.extension.sse.ui.httppanel.models.StringEventStreamPanelViewModel;
+import org.zaproxy.zap.extension.sse.ui.httppanel.views.EventStreamSyntaxHighlightTextView;
+import org.zaproxy.zap.extension.sse.ui.httppanel.views.large.EventStreamLargeEventUtil;
+import org.zaproxy.zap.extension.sse.ui.httppanel.views.large.EventStreamLargePayloadView;
+import org.zaproxy.zap.extension.sse.ui.httppanel.views.large.EventStreamLargetPayloadViewModel;
+import org.zaproxy.zap.view.HttpPanelManager;
+import org.zaproxy.zap.view.HttpPanelManager.HttpPanelComponentFactory;
+import org.zaproxy.zap.view.HttpPanelManager.HttpPanelDefaultViewSelectorFactory;
+import org.zaproxy.zap.view.HttpPanelManager.HttpPanelViewFactory;
+ 
+/**
+ * The Server-Sent Events (SSE) extension was written in December 2012. This
+ * specification defines an API for opening an HTTP connection for receiving
+ * push notifications from a server in the form of DOM events. See:
+ * http://www.w3.org/TR/eventsource/.
+ * 
+ * @author Robert Koch
+ */
+public class ExtensionServerSentEvents extends ExtensionAdaptor implements PersistentConnectionListener, SessionChangedListener {
+    
+	private static final Logger logger = Logger.getLogger(ExtensionServerSentEvents.class);
+	public static final int HANDSHAKE_LISTENER = 10;
+	
+	/**
+	 * Name of this extension.
+	 */
+	public static final String NAME = "ExtensionServerSentEvents";
+	
+	private Charset charset;
+	
+	/**
+	 * Responsible for storing events.
+	 */
+	private EventStreamStorage storage;
+	
+	/**
+	 * List of observers added to all event streams.
+	 */
+	private List<EventStreamObserver> observers = new ArrayList<>();
+	private Map<Integer, EventStreamProxy> sseProxies = new HashMap<>();
+	private EventStreamPanel panel;
+
+	
+	public ExtensionServerSentEvents() {
+		super(NAME);
+		
+		charset = Charset.forName("UTF-8");
+		
+		setOrder(160);
+	}
+	
+	@Override
+	public void init() {
+		super.init();
+		
+		// setup database
+		storage = new EventStreamStorage(createDatabaseTable());
+		addObserver(storage);
+	}
+	
+	private void addObserver(EventStreamObserver observer) {
+		synchronized (observers) {
+			observers.add(observer);
+		}
+	}
+
+	@Override
+	public void hook(ExtensionHook extensionHook) {
+		super.hook(extensionHook);
+		ExtensionHookView hookView = extensionHook.getHookView();
+		
+		extensionHook.addPersistentConnectionListener(this);
+		
+		extensionHook.addSessionListener(this);
+		
+		// setup SSE tab
+		EventStreamPanel tab = getEventStreamTab();
+		tab.setDisplayPanel(getView().getRequestPanel(), getView().getResponsePanel());
+		
+		extensionHook.addSessionListener(tab.getSessionListener());
+		
+		addObserver(tab);
+		
+		hookView.addStatusPanel(tab);
+		
+		initializeWorkPanel();
+		
+        ExtensionHelp.enableHelpKey(tab, "sse.tab");
+	}
+
+	private EventStreamPanel getEventStreamTab() {
+		if (panel == null) {
+			panel = new EventStreamPanel(storage.getTable());//TODO, getBrkManager());
+		}
+		return panel;
+	}
+	
+	private TableEventStream createDatabaseTable() {
+		TableEventStream table = new TableEventStream();
+		Database db = Model.getSingleton().getDb();
+		db.addDatabaseListener(table);
+		try {
+			table.databaseOpen(db.getDatabaseServer());
+		} catch (SQLException e) {
+			logger.warn(e.getMessage(), e);
+		}
+		return table;
+	}
+
+	@Override
+	public String getAuthor() {
+		return Constant.ZAP_TEAM;
+	}
+	
+	@Override
+	public String getDescription() {
+		return Constant.messages.getString("sse.desc");
+	}
+
+	/**
+	 * Add Server-Sent Events stream.
+	 * 
+	 * @param msg Contains request & response headers.
+	 * @param remoteReader Content arrives continuously and is forwarded to local client.
+	 * @param localWriter Received content is written here.
+	 */
+	public void addEventStream(HttpMessage msg, final InputStream remoteReader, final OutputStream localWriter) {
+		BufferedReader reader = new BufferedReader(new InputStreamReader(remoteReader, charset));
+		BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(localWriter, charset));
+		
+		EventStreamProxy proxy = new EventStreamProxy(msg, reader, writer);
+		synchronized (observers) {
+			for (EventStreamObserver observer : observers) {
+				proxy.addObserver(observer);
+			}
+		}
+		proxy.start();
+		//TODO: save all proxies
+	}
+
+	@Override
+	public int getArrangeableListenerOrder() {
+		return HANDSHAKE_LISTENER;
+	}
+
+	@Override
+	public boolean onHandshakeResponse(HttpMessage httpMessage, Socket inSocket, ZapGetMethod method) {
+		boolean keepSocketOpen = false;
+		
+		if (httpMessage.isEventStream()) {
+			logger.debug("Got Server-Sent Events stream.");
+
+			ZapGetMethod handshakeMethod = (ZapGetMethod) httpMessage.getUserObject();
+			if (handshakeMethod != null) {
+				keepSocketOpen = true;
+				
+				InputStream inputStream;
+				try {
+					inputStream = handshakeMethod.getResponseBodyAsStream();
+
+					inSocket.setSoTimeout(0);
+					inSocket.setTcpNoDelay(true);
+					inSocket.setKeepAlive(true);
+					
+					addEventStream(httpMessage, inputStream, inSocket.getOutputStream());
+				} catch (IOException e) {
+					logger.warn(e.getMessage(), e);
+					keepSocketOpen = false;
+				}
+			} else {
+				logger.error("Was not able to retrieve input stream.");
+			}
+		}
+		
+		return keepSocketOpen;
+	}
+
+	@Override
+	public void sessionChanged(final Session session) {
+		TableEventStream table = createDatabaseTable();
+//		if (View.isInitialised()) {
+//			getWebSocketPanel().setTable(table);
+//		}
+		storage.setTable(table);
+		
+//		try {
+//			WebSocketProxy.setChannelIdGenerator(table.getMaxChannelId());
+//		} catch (SQLException e) {
+//			logger.error("Unable to retrieve current channelId value!", e);
+//		}
+	}
+
+	@Override
+	public void sessionAboutToChange(Session session) {
+		if (View.isInitialised()) {
+			// Prevent the table from being used
+//			getWebSocketPanel().setTable(null);
+			storage.setTable(null);
+		}
+		
+		// close existing connections
+		synchronized (sseProxies) {
+			for (EventStreamProxy sseProxy : sseProxies.values()) {
+				sseProxy.stop();
+			}
+			sseProxies.clear();
+		}
+	}
+
+
+	@Override
+	public void sessionScopeChanged(Session session) {
+		
+	}
+
+	@Override
+	public void sessionModeChanged(Mode mode) {
+		
+	}
+
+	public static int compareTo(EventStreamObserver base, EventStreamObserver other) {
+		int myOrder = base.getServerSentEventObservingOrder();
+		int otherOrder = other.getServerSentEventObservingOrder();
+		
+		if (myOrder < otherOrder) {
+			return -1;
+		} else if (myOrder > otherOrder){
+			return 1;
+		}
+		
+		return 0;
+	}
+
+	private void initializeWorkPanel() {
+		// Add "HttpPanel" components and views.
+		HttpPanelManager manager = HttpPanelManager.getInstance();
+
+		// component factory for outgoing and incoming messages with Text view
+		HttpPanelComponentFactory componentFactory = new EventStreamComponentFactory();
+		manager.addResponseComponent(componentFactory);
+
+		// use same factory for request & response,
+		// as Hex payloads are accessed the same way
+		HttpPanelViewFactory viewFactory = new EventStreamHexViewFactory();
+		manager.addResponseView(EventStreamComponent.NAME, viewFactory);
+		
+		// add the default Hex view for binary-opcode messages
+		HttpPanelDefaultViewSelectorFactory viewSelectorFactory = new HexDefaultViewSelectorFactory();
+		manager.addResponseDefaultView(EventStreamComponent.NAME, viewSelectorFactory);
+
+		// replace the normal Text views with the ones that use syntax highlighting
+		viewFactory = new SyntaxHighlightTextViewFactory();
+		manager.addResponseView(EventStreamComponent.NAME, viewFactory);
+
+		// support large payloads on incoming and outgoing messages
+		viewFactory = new EventStreamLargePayloadViewFactory();
+		manager.addResponseView(EventStreamComponent.NAME, viewFactory);
+		
+		viewSelectorFactory = new EventStreamLargeEventDefaultViewSelectorFactory();
+		manager.addResponseDefaultView(EventStreamComponent.NAME, viewSelectorFactory);
+	}
+
+	/**
+	 * The component returned by this factory contain the normal text view
+	 * (without syntax highlighting).
+	 */
+    private static final class EventStreamComponentFactory implements HttpPanelComponentFactory {
+        
+        @Override
+        public HttpPanelComponentInterface getNewComponent() {
+            return new EventStreamComponent();
+        }
+
+        @Override
+        public String getComponentName() {
+            return EventStreamComponent.NAME;
+        }
+    }
+	
+	private static final class EventStreamHexViewFactory implements HttpPanelViewFactory {
+        
+        @Override
+        public HttpPanelView getNewView() {
+            return new HttpPanelHexView(new ByteEventStreamPanelViewModel(), false);
+        }
+
+        @Override
+        public Object getOptions() {
+            return null;
+        }
+    }
+	
+	private static final class HexDefaultViewSelector implements HttpPanelDefaultViewSelector {
+
+        @Override
+        public String getName() {
+            return "HexDefaultViewSelector";
+        }
+        
+        @Override
+        public boolean matchToDefaultView(Message aMessage) {
+        	// use hex view only when previously selected
+//            if (aMessage instanceof WebSocketMessageDTO) {
+//                WebSocketMessageDTO msg = (WebSocketMessageDTO)aMessage;
+//                
+//                return (msg.opcode == WebSocketMessage.OPCODE_BINARY);
+//            }
+            return false;
+        }
+
+        @Override
+        public String getViewName() {
+            return HttpPanelHexView.CONFIG_NAME;
+        }
+        
+        @Override
+        public int getOrder() {
+            return 20;
+        }
+    }
+
+    private static final class HexDefaultViewSelectorFactory implements HttpPanelDefaultViewSelectorFactory {
+        
+        private static HttpPanelDefaultViewSelector defaultViewSelector = null;
+        
+        private HttpPanelDefaultViewSelector getDefaultViewSelector() {
+            if (defaultViewSelector == null) {
+                createViewSelector();
+            }
+            return defaultViewSelector;
+        }
+        
+        private synchronized void createViewSelector() {
+            if (defaultViewSelector == null) {
+                defaultViewSelector = new HexDefaultViewSelector();
+            }
+        }
+        
+        @Override
+        public HttpPanelDefaultViewSelector getNewDefaultViewSelector() {
+            return getDefaultViewSelector();
+        }
+        
+        @Override
+        public Object getOptions() {
+            return null;
+        }
+    }
+	
+    private static final class SyntaxHighlightTextViewFactory implements HttpPanelViewFactory {
+        
+        @Override
+        public HttpPanelView getNewView() {
+            return new EventStreamSyntaxHighlightTextView(new StringEventStreamPanelViewModel());
+        }
+        
+        @Override
+        public Object getOptions() {
+            return null;
+        }
+    }
+	
+	private static final class EventStreamLargePayloadViewFactory implements HttpPanelViewFactory {
+		
+		@Override
+		public HttpPanelView getNewView() {
+			return new EventStreamLargePayloadView(new EventStreamLargetPayloadViewModel());
+		}
+
+		@Override
+		public Object getOptions() {
+			return null;
+		}
+	}
+	
+	private static final class EventStreamLargeEventDefaultViewSelectorFactory implements HttpPanelDefaultViewSelectorFactory {
+		
+		private static HttpPanelDefaultViewSelector defaultViewSelector = null;
+		
+		private HttpPanelDefaultViewSelector getDefaultViewSelector() {
+			if (defaultViewSelector == null) {
+				createViewSelector();
+			}
+			return defaultViewSelector;
+		}
+		
+		private synchronized void createViewSelector() {
+			if (defaultViewSelector == null) {
+				defaultViewSelector = new EventStreamLargePayloadDefaultViewSelector();
+			}
+		}
+		
+		@Override
+		public HttpPanelDefaultViewSelector getNewDefaultViewSelector() {
+			return getDefaultViewSelector();
+		}
+		
+		@Override
+		public Object getOptions() {
+			return null;
+		}
+	}
+
+	private static final class EventStreamLargePayloadDefaultViewSelector implements HttpPanelDefaultViewSelector {
+
+		@Override
+		public String getName() {
+			return "EventStreamLargePayloadDefaultViewSelector";
+		}
+		
+		@Override
+		public boolean matchToDefaultView(Message aMessage) {
+		    return EventStreamLargeEventUtil.isLargeEvent(aMessage);
+		}
+
+		@Override
+		public String getViewName() {
+			return EventStreamLargePayloadView.CONFIG_NAME;
+		}
+        
+        @Override
+        public int getOrder() {
+        	// has to come before HexDefaultViewSelector
+            return 15;
+        }
+	}
+}
