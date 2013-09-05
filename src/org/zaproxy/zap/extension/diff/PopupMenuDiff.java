6c02eae588afaf622773f99343b1fe3fd14d5b6c
psiinon@gmail.com <psiinon@users.noreply.github.com>
2013-09-05 13:56:41 +0000

    Moved diff addon to beta, support requests and added (basic) help

---
+++ src/org/zaproxy/zap/extension/diff/PopupMenuDiff.java
@@ -0,0 +1,73 @@
+/*
+ * Zed Attack Proxy (ZAP) and its related class files.
+ * 
+ * ZAP is an HTTP/HTTPS proxy for assessing web application security.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); 
+ * you may not use this file except in compliance with the License. 
+ * You may obtain a copy of the License at 
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0 
+ *   
+ * Unless required by applicable law or agreed to in writing, software 
+ * distributed under the License is distributed on an "AS IS" BASIS, 
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+ * See the License for the specific language governing permissions and 
+ * limitations under the License. 
+ */
+package org.zaproxy.zap.extension.diff;
+
+import java.util.List;
+
+import org.parosproxy.paros.model.HistoryReference;
+import org.zaproxy.zap.view.PopupMenuHistoryReference;
+
+public class PopupMenuDiff extends PopupMenuHistoryReference {
+
+	private static final long serialVersionUID = 1L;
+    private ExtensionDiff ext = null;
+    private boolean request = false;
+
+    /**
+     * @param label
+     */
+    public PopupMenuDiff(String label, ExtensionDiff ext, boolean request) {
+        super(label, true);
+        this.setName("DiffPopup");
+        this.ext = ext;
+        this.request = request;
+    }
+
+
+    public boolean isEnableForInvoker(Invoker invoker) {
+    	return true;
+    }
+
+	@Override
+    public boolean isEnabledForHistoryReferences (List<HistoryReference> hrefs) {
+	    if (hrefs.size() != 2) {
+	        return false;
+	    }
+	    
+	    for (HistoryReference hRef : hrefs) {
+	        if (hRef == null) {
+	            return false;
+	        }
+	    }
+	    
+    	return true;
+    }
+
+	@Override
+    public void performActions (List<HistoryReference> hrefs) throws Exception {
+    	if (hrefs.size() == 2) {
+    		this.ext.showDiffDialog(hrefs.get(0).getHttpMessage(), hrefs.get(1).getHttpMessage(), this.request);
+    	}
+    }
+
+
+	@Override
+	public void performAction(HistoryReference href) throws Exception {
+		// Ignore
+	}
+}
