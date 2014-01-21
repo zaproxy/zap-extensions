c3e1808b122305061cb24be9df345ca7d0c3baed
yhawke@gmail.com <yhawke@gmail.com@1e26c7e7-97f5-7f45-da66-271314645f9b>
2014-01-21 22:37:23 +0000

    Revision: r903 Added i18N, attackStrenght, evidence and some minor issues on CommandInjectionPlugin

--- branches/beta/src/org/zaproxy/zap/extension/ascanrulesBeta/CommandInjectionPlugin.java
+++ branches/beta/src/org/zaproxy/zap/extension/ascanrulesBeta/CommandInjectionPlugin.java
@@ -12,17 +12,19 @@
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
+ * See the License for -the specific language governing permissions and
  * limitations under the License.
  */
 package org.zaproxy.zap.extension.ascanrulesBeta;
 
 import java.io.IOException;
-import java.util.HashMap;
+import java.util.Iterator;
+import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import org.apache.log4j.Logger;
+import org.parosproxy.paros.Constant;
 import org.parosproxy.paros.core.scanner.AbstractAppParamPlugin;
 import org.parosproxy.paros.core.scanner.Alert;
 import org.parosproxy.paros.core.scanner.Category;
@@ -52,8 +54,14 @@ public class CommandInjectionPlugin extends AbstractAppParamPlugin {
     private static final String BASH_SPACE_REPLACEMENT = "${IFS}";
     
     // OS Command payloads for command Injection testing
-    private static final Map<String, Pattern> OS_PAYLOADS = new HashMap();
+    private static final Map<String, Pattern> OS_PAYLOADS = new LinkedHashMap();
     static {
+        // No quote payloads
+        OS_PAYLOADS.put("&" + NIX_TEST_CMD + "&", NIX_CTRL_PATTERN);
+        OS_PAYLOADS.put(";" + NIX_TEST_CMD + ";", NIX_CTRL_PATTERN);
+        OS_PAYLOADS.put("&" + WIN_TEST_CMD, WIN_CTRL_PATTERN);
+        OS_PAYLOADS.put("|" + WIN_TEST_CMD, WIN_CTRL_PATTERN);
+        
         // Double quote payloads
         OS_PAYLOADS.put("\"&" + NIX_TEST_CMD + "&\"", NIX_CTRL_PATTERN);
         OS_PAYLOADS.put("\";" + NIX_TEST_CMD + ";\"", NIX_CTRL_PATTERN);
@@ -64,11 +72,7 @@ public class CommandInjectionPlugin extends AbstractAppParamPlugin {
         OS_PAYLOADS.put("';" + NIX_TEST_CMD + ";'", NIX_CTRL_PATTERN);
         OS_PAYLOADS.put("'&" + WIN_TEST_CMD + "&'", WIN_CTRL_PATTERN);
         OS_PAYLOADS.put("'|" + WIN_TEST_CMD, WIN_CTRL_PATTERN);
-        // No quote payloads
-        OS_PAYLOADS.put("&" + NIX_TEST_CMD + "&", NIX_CTRL_PATTERN);
-        OS_PAYLOADS.put(";" + NIX_TEST_CMD + ";", NIX_CTRL_PATTERN);
-        OS_PAYLOADS.put("&" + WIN_TEST_CMD, WIN_CTRL_PATTERN);
-        OS_PAYLOADS.put("|" + WIN_TEST_CMD, WIN_CTRL_PATTERN);
+        
         // Special payloads   
         OS_PAYLOADS.put("\n" + NIX_TEST_CMD + "\n", NIX_CTRL_PATTERN);  //force enter
         OS_PAYLOADS.put("`" + NIX_TEST_CMD + "`", NIX_CTRL_PATTERN);    //backtick execution
@@ -106,7 +110,7 @@ public class CommandInjectionPlugin extends AbstractAppParamPlugin {
      */
     @Override
     public String getName() {
-        return "Remote OS Command Injection Plugin";
+        return Constant.messages.getString("ascanbeta.cmdinjection.name");
     }
     
     /**
@@ -124,10 +128,7 @@ public class CommandInjectionPlugin extends AbstractAppParamPlugin {
      */
     @Override
     public String getDescription() {
-        return "Attack technique used for unauthorized execution of operating system commands. "
-                + "This attack is possible when an application accepts untrusted input to build "
-                + "operating system commands in an insecure manner involving improper data sanitization, "
-                + "and/or improper calling of external programs.";
+        return Constant.messages.getString("ascanbeta.cmdinjection.desc");
     }
 
     /**
@@ -216,12 +217,41 @@ public class CommandInjectionPlugin extends AbstractAppParamPlugin {
                     + "], parameter [" + paramName 
                     + "] for OS Command Injection vulnerabilites");
         }
+        
+        // Number of targets to try
+        int targetCount = 0;
 
+        switch (this.getAttackStrength()) {
+            case LOW:
+                // This works out as a total of 4 reqs / param
+                targetCount = 4;
+                break;
+
+            case MEDIUM:
+                // This works out as a total of 12 reqs / param
+                targetCount = 12;
+                break;
+
+            case HIGH:
+            case INSANE:
+                // This works out as a total of 18 reqs / param
+                targetCount = OS_PAYLOADS.size();
+                break;
+
+            default:
+            // Default to off
+        }
+        
         // ------------------------------------------
-        // Start testing PHP Code Injection patterns
+        // Start testing OS Command Injection patterns
         // ------------------------------------------
-        for (String payload : OS_PAYLOADS.keySet()) {
+        String payload;
+        Iterator<String> it = OS_PAYLOADS.keySet().iterator();
+        
+        for(int i = 0; it.hasNext() && (i < targetCount); i++) {
+
             msg = getNewMsg();
+            payload = it.next();
             setParameter(msg, paramName, payload);
 
             if (log.isDebugEnabled()) {
@@ -244,13 +274,11 @@ public class CommandInjectionPlugin extends AbstractAppParamPlugin {
                     this.bingo(
                             Alert.RISK_HIGH, 
                             Alert.WARNING, 
-                            getName(),
-                            getDescription(),
                             null,
                             paramName,
                             payload, 
                             null,
-                            getSolution(),
+                            matcher.group(),
                             msg);
 
                     // All done. No need to look for vulnerabilities on subsequent 
@@ -261,7 +289,7 @@ public class CommandInjectionPlugin extends AbstractAppParamPlugin {
             } catch (IOException ex) {
                 //Do not try to internationalise this.. we need an error message in any event..
                 //if it's in English, it's still better than not having it at all.
-                log.error("PHP Code Injection vulnerability check failed for parameter ["
+                log.error("PCommand Injection vulnerability check failed for parameter ["
                     + paramName + "] and payload [" + payload + "] due to an I/O error", ex);
             }
             
