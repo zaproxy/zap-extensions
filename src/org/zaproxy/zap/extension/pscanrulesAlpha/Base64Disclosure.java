b1f739413831fe4f4b07f869a535bb0bf1ee8cfc
70pointer@gmail.com <70pointer@gmail.com>
2014-03-20 19:27:00 +0000

    Updated the Base64Disclosure scanner to also passively scan for ASP.NET ViewState issues, since the ViewState is Base64 encoded.

--- src/org/zaproxy/zap/extension/pscanrulesAlpha/Base64Disclosure.java
+++ src/org/zaproxy/zap/extension/pscanrulesAlpha/Base64Disclosure.java
@@ -36,15 +36,14 @@ import org.zaproxy.zap.extension.pscan.PluginPassiveScanner;
 
 
 /**
- * A class to passively scan responses for Base64 encoded data 
+ * A class to passively scan responses for Base64 encoded data, including ASP ViewState data, which is Base64 encoded.  
  * @author 70pointer@gmail.com
  *
  */
-
 public class Base64Disclosure extends PluginPassiveScanner {
-	
+
 	private PassiveScanThread parent = null;
-	
+
 	/**
 	 * a pattern used to identify Base64 encoded data. Set a minimum length to reduce false positives.
 	 * Note that because we only look for patterns ending in at least one "=", we will have false negatives (ie, we will not detect ALL Base64 references).
@@ -52,10 +51,9 @@ public class Base64Disclosure extends PluginPassiveScanner {
 	 * TODO: find a different way to reduce false positives without causing false negatives.
 	 */
 	static Pattern base64Pattern = Pattern.compile("[a-zA-Z0-9\\+\\\\/]{30,}={1,2}");
-	
-	
-	private static Logger log = Logger.getLogger(Base64Disclosure.class);
 
+	private static Logger log = Logger.getLogger(Base64Disclosure.class);
+	
 	/**
 	 * Prefix for internationalized messages used by this rule
 	 */
@@ -77,6 +75,7 @@ public class Base64Disclosure extends PluginPassiveScanner {
 	public String getName() {
 		return Constant.messages.getString(MESSAGE_PREFIX + "name");
 	}
+	
 
 	/**
 	 * scans the HTTP request sent (in fact, does nothing)
@@ -96,48 +95,122 @@ public class Base64Disclosure extends PluginPassiveScanner {
 	 */
 	@Override
 	public void scanHttpResponseReceive(HttpMessage msg, int id, Source source) {
-		
+
 		if (log.isDebugEnabled()) log.debug("Checking message "+ msg + " for Base64 encoded data");
-		
+
 		//get the body contents as a String, so we can match against it
 		String responseheader = msg.getResponseHeader().getHeadersAsString();
 		String responsebody = new String (msg.getResponseBody().getBytes());
 		String [] responseparts = {responseheader, responsebody};
-				
+
 		if (log.isDebugEnabled()) log.debug("Trying Base64 Pattern: "+ base64Pattern );
 		for (String haystack: responseparts) {
 			Matcher matcher = base64Pattern.matcher(haystack);
-	        while (matcher.find()) {
-	            String evidence = matcher.group();
-	            byte[] decodeddata=null;
-        		try {
-        			//decode the data
-        			decodeddata = Base64.decode(evidence);             	
-	            } catch (IOException e) {
-	            	//it's not actually Base64. so skip it.
+			while (matcher.find()) {
+				String evidence = matcher.group();
+				byte[] decodeddata=null;
+				try {
+					//decode the data
+					decodeddata = Base64.decode(evidence);             	
+				} catch (IOException e) {
+					//it's not actually Base64. so skip it.
+					if (log.isDebugEnabled()) log.debug("["+evidence + "] could not be decoded as Base64 data");
 					continue;
 				}
-	            if (log.isDebugEnabled()) log.debug("Found a match for Base64:" + evidence);
-	            
-		        if ( evidence!=null && evidence.length() > 0) {
-					//we found something
-					Alert alert = new Alert(getId(), Alert.RISK_INFO, Alert.WARNING, getName() );
+				if (log.isDebugEnabled()) log.debug("Found a match for Base64:" + evidence);
+
+				//so it's valid Base64.  Is it valid .NET ViewState data?
+				//This will be true for both __VIEWSTATE and __EVENTVALIDATION data, although currently, we can only interpret/decode __VIEWSTATE.
+				boolean validviewstate = false;
+				boolean macless = false;
+				String viewstatexml = null;
+				if ( decodeddata[0]==-1 || decodeddata[1]==0x01 ) {
+					//TODO: decode __EVENTVALIDATION data
+					ViewStateDecoder viewstatedecoded = new ViewStateDecoder ();					
+					try {
+						if (log.isDebugEnabled()) log.debug("The following Base64 string has a ViewState preamble: ["+evidence + "]");
+						viewstatexml = viewstatedecoded.decode(evidence.getBytes());
+						if (log.isDebugEnabled()) log.debug("The data was successfully decoded as ViewState data");
+						validviewstate = true;
+
+						//is the ViewState protected by a MAC?
+						Matcher hmaclessmatcher = ViewStateDecoder.patternNoHMAC.matcher(viewstatexml);
+						macless = hmaclessmatcher.find();
+						
+						if (log.isDebugEnabled()) log.debug("MACles??? "+ macless);
+					} catch (Exception e) {
+						//no need to do anything here.. just don't set "validviewstate" to true :)
+						e.printStackTrace();
+						if (log.isDebugEnabled()) log.debug("The Base64 value ["+ evidence+"] has a valid ViewState pre-amble, but is not a valid viewstate. It may be an EVENTVALIDATION value, is not yet decodable.");
+					} 
+				} 
+
+				if (validviewstate == true) {
+					if (log.isDebugEnabled()) log.debug("Raising a ViewState informational alert");
+					
+					//raise an (informational) Alert with the human readable ViewState data
+					Alert alert = new Alert(getId(), Alert.RISK_INFO, Alert.WARNING, Constant.messages.getString("pscanalpha.base64disclosure.viewstate.name"));
 					alert.setDetail(
-							getDescription(), 
+							Constant.messages.getString("pscanalpha.base64disclosure.viewstate.desc"), 
 							msg.getRequestHeader().getURI().toString(), 
 							"", //param
-							evidence, //TODO: this should be the the attack (NULL).  Set this field to NULL, once Zap allows mutiple alerts on the same URL, with just different evidence 
-							getExtraInfo(msg, evidence, decodeddata),  //other info
-							getSolution(), 
-							getReference(), 
-							evidence,
+							viewstatexml, //TODO: this should be the the attack (NULL).  Set this field to NULL, once Zap allows mutiple alerts on the same URL, with just different evidence 
+							Constant.messages.getString("pscanalpha.base64disclosure.viewstate.extrainfo", viewstatexml), //other info
+							Constant.messages.getString("pscanalpha.base64disclosure.viewstate.soln"), 
+							Constant.messages.getString("pscanalpha.base64disclosure.viewstate.refs"), 
+							viewstatexml,
 							200, //Information Exposure, 
 							13, //Information Leakage
 							msg);  
 					parent.raiseAlert(id, alert);
-					//do NOT break at this point.. we need to find *all* the potential Base64 encoded data in the response..
-		        }
-	        }
+					//do NOT break at this point.. we need to find *all* the issues
+					
+					//if the ViewState is not protected by a MAC, alert it as a High, cos we can mess with the parameters for sure..
+					if ( macless ) {
+						Alert alertmacless = new Alert(getId(), Alert.RISK_HIGH, Alert.WARNING, Constant.messages.getString("pscanalpha.base64disclosure.viewstatewithoutmac.name"));
+						alertmacless.setDetail(
+								Constant.messages.getString("pscanalpha.base64disclosure.viewstatewithoutmac.desc"), 
+								msg.getRequestHeader().getURI().toString(), 
+								"", //param
+								viewstatexml, //TODO: this should be the the attack (NULL).  Set this field to NULL, once Zap allows mutiple alerts on the same URL, with just different evidence 
+								Constant.messages.getString("pscanalpha.base64disclosure.viewstatewithoutmac.extrainfo", viewstatexml), //other info
+								Constant.messages.getString("pscanalpha.base64disclosure.viewstatewithoutmac.soln"), 
+								Constant.messages.getString("pscanalpha.base64disclosure.viewstatewithoutmac.refs"), 
+								viewstatexml,
+								642, //CWE-642 = External Control of Critical State Data 
+								13, //Information Leakage
+								msg);  
+						parent.raiseAlert(id, alertmacless);
+						//do NOT break at this point.. we need to find *all* the issues
+					}
+					
+					
+					//TODO: if the ViewState contains sensitive data, alert it (particularly if running over HTTP)					
+					} 
+				else {
+					if (log.isDebugEnabled()) log.debug("Raising a Base64 informational alert");
+					
+					//the Base64 decoded data is not a valid ViewState (even though it may have a valid ViewStatet pre-amble) 
+					//so treat it as normal Base64 data, and raise an informational alert.
+					if ( evidence!=null && evidence.length() > 0) {
+						Alert alert = new Alert(getId(), Alert.RISK_INFO, Alert.WARNING, getName() );
+						alert.setDetail(
+								getDescription(), 
+								msg.getRequestHeader().getURI().toString(), 
+								"", //param
+								evidence, //TODO: this should be the the attack (NULL).  Set this field to NULL, once Zap allows mutiple alerts on the same URL, with just different evidence 
+								getExtraInfo(msg, evidence, decodeddata),  //other info
+								getSolution(), 
+								getReference(), 
+								evidence,
+								200, //Information Exposure, 
+								13, //Information Leakage
+								msg);  
+						parent.raiseAlert(id, alert);
+						//do NOT break at this point.. we need to find *all* the potential Base64 encoded data in the response..
+					}
+				}
+			}
 		}	
 
 	}
