23bb82b01d17eeeadb6b1f109d1066b6c2202933
THC202@gmail.com <THC202@gmail.com@1e26c7e7-97f5-7f45-da66-271314645f9b>
2014-03-26 08:59:00 +0000

    Issue 609 - Provide a common interface to query the state and access the data (HttpMessage and HistoryReference) displayed in the tabs
     - Changed the popup menu item to use the added (core) pop up menu item (HttpMessageContainer);
     - Updated "changes" element of the ZapAddOn.xml file.

--- branches/alpha/src/org/zaproxy/zap/extension/saml/SAMLResendMenuItem.java
+++ branches/alpha/src/org/zaproxy/zap/extension/saml/SAMLResendMenuItem.java
@@ -1,28 +1,36 @@
 package org.zaproxy.zap.extension.saml;
 
+import org.apache.log4j.Logger;
 import org.parosproxy.paros.network.HttpMessage;
 import org.parosproxy.paros.view.View;
 import org.zaproxy.zap.extension.saml.ui.SamlManualEditor;
-import org.zaproxy.zap.view.PopupMenuHttpMessage;
+import org.zaproxy.zap.view.messagecontainer.http.HttpMessageContainer;
+import org.zaproxy.zap.view.popup.PopupMenuItemHttpMessageContainer;
 
-public class SAMLResendMenuItem extends PopupMenuHttpMessage {
+public class SAMLResendMenuItem extends PopupMenuItemHttpMessageContainer {
+
+    private static final Logger LOGGER = Logger.getLogger(SAMLResendMenuItem.class); 
 
     public SAMLResendMenuItem(String label) {
         super(label);
     }
 
     @Override
-    public void performAction(HttpMessage httpMessage) throws Exception {
+    public void performAction(HttpMessage httpMessage) {
         if (!SAMLUtils.hasSAMLMessage(httpMessage)) {
             View.getSingleton().showWarningDialog("Not a valid SAML request");
             return;
         }
-        SamlManualEditor editor = new SamlManualEditor(new SAMLMessage(httpMessage));
-        editor.setVisible(true);
+        try {
+            SamlManualEditor editor = new SamlManualEditor(new SAMLMessage(httpMessage));
+            editor.setVisible(true);
+        } catch (SAMLException e) {
+            LOGGER.error("Failed to show SAML manual editor: " + e.getMessage(), e);
+        }
     }
 
     @Override
-    public boolean isEnableForInvoker(Invoker invoker) {
+    public boolean isEnableForInvoker(Invoker invoker, HttpMessageContainer httpMessageContainer) {
         //TODO filter out the unnecessary invokers
         return true;
     }
