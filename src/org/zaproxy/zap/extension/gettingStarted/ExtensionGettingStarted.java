6be1906ca2a624c0b89f13476cac29e71d4b97da
bjoern.kimminich@gmx.de <bjoern.kimminich@gmx.de>
2014-05-02 07:24:20 +0000

    some small code and comment cleanups

--- src/org/zaproxy/zap/extension/gettingStarted/ExtensionGettingStarted.java
+++ src/org/zaproxy/zap/extension/gettingStarted/ExtensionGettingStarted.java
@@ -17,108 +17,97 @@
  */
 package org.zaproxy.zap.extension.gettingStarted;
 
-import java.awt.Desktop;
-import java.io.File;
-import java.net.MalformedURLException;
-import java.net.URL;
-
 import org.apache.log4j.Logger;
 import org.parosproxy.paros.Constant;
 import org.parosproxy.paros.extension.ExtensionAdaptor;
 import org.parosproxy.paros.extension.ExtensionHook;
 import org.zaproxy.zap.view.ZapMenuItem;
 
-/*
- * An example ZAP extension which adds a top level menu item. 
- * 
- * This class is defines the extension.
+import java.awt.*;
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+/**
+ * An example ZAP extension which adds a top level menu item.
  */
 public class ExtensionGettingStarted extends ExtensionAdaptor {
 
-	private static final String DIR = "lang";
-    private ZapMenuItem menuGettingStarted = null;
-    private Logger logger = Logger.getLogger(getClass());
+    private static Logger logger = Logger.getLogger(ExtensionGettingStarted.class);
 
-	/**
-     * 
-     */
+    private static final String DIR = "lang";
+
+    private ZapMenuItem menuGettingStarted = null;
+
     public ExtensionGettingStarted() {
-        super();
- 		initialize();
+        this("ExtensionGettingStarted");
     }
 
-    /**
-     * @param name
-     */
     public ExtensionGettingStarted(String name) {
         super(name);
+        initialize();
     }
 
-	/**
-	 * This method initializes this
-	 * 
-	 */
-	private void initialize() {
-        this.setName("ExtensionGettingStarted");
-	}
-	
-	@Override
-	public void hook(ExtensionHook extensionHook) {
-	    super.hook(extensionHook);
-	    
-	    if (getView() != null) {
-	    	// Register our top menu item, as long as we're not running as a daemon
-	    	// Use one of the other methods to add to a different menu list
-	        extensionHook.getHookMenu().addHelpMenuItem(getMenuGettingStarted());
-	    }
+    private void initialize() {
+        // TODO Initialize extension upon creation. If no initialization is required, delete method
+    }
 
-	}
+    @Override
+    public void hook(ExtensionHook extensionHook) {
+        super.hook(extensionHook);
+        if (getView() != null) {
+            /* Register our top menu item, as long as we're not running as a daemon
+             * Use one of the other methods to add to a different menu list
+             */
+            extensionHook.getHookMenu().addHelpMenuItem(getMenuGettingStarted());
+        }
 
-	private ZapMenuItem getMenuGettingStarted() {
+    }
+
+    private ZapMenuItem getMenuGettingStarted() {
         if (menuGettingStarted == null) {
-        	menuGettingStarted = new ZapMenuItem("gettingStarted.menu");
-        	menuGettingStarted.addActionListener(new java.awt.event.ActionListener() {
+            menuGettingStarted = new ZapMenuItem("gettingStarted.menu");
+            menuGettingStarted.addActionListener(new java.awt.event.ActionListener() {
                 @Override
-                public void actionPerformed(java.awt.event.ActionEvent e) {
-                	try {
-                		/*
-                		 * Note that if you translate the guide to another language you need to also change
+                public void actionPerformed(java.awt.event.ActionEvent event) {
+                    try {
+                        /* Note that if you translate the guide to another language you need to also change
                 		 * the language file so that gettingStarted.file refers to the localized file name
                 		 */
-                		File guide = new File(Constant.getZapHome() + File.separator + DIR + File.separator + 
-                				Constant.messages.getString("gettingStarted.file"));
-                		logger.debug("Getting started guide: " + guide.getAbsolutePath());
-						Desktop.getDesktop().open(guide);
-					} catch (Exception e1) {
-						logger.error(e1.getMessage(), e1);
-					}
+                        File guide = new File(Constant.getZapHome() + File.separator + DIR + File.separator +
+                                Constant.messages.getString("gettingStarted.file"));
+                        logger.debug("Getting started guide: " + guide.getAbsolutePath());
+                        Desktop.getDesktop().open(guide);
+                    } catch (Exception ex) {
+                        logger.error("Failed to locate or open Getting started guide: ", ex);
+                    }
                 }
             });
         }
         return menuGettingStarted;
     }
 
-	@Override
-	public boolean canUnload() {
-		return true;
-	}
-	
-	@Override
-	public String getAuthor() {
-		return Constant.ZAP_TEAM;
-	}
+    @Override
+    public boolean canUnload() {
+        return true;
+    }
 
-	@Override
-	public String getDescription() {
-		return Constant.messages.getString("gettingStarted.desc");
-	}
+    @Override
+    public String getAuthor() {
+        return Constant.ZAP_TEAM;
+    }
 
-	@Override
-	public URL getURL() {
-		try {
-			return new URL(Constant.ZAP_EXTENSIONS_PAGE);
-		} catch (MalformedURLException e) {
-			return null;
-		}
-	}
+    @Override
+    public String getDescription() {
+        return Constant.messages.getString("gettingStarted.desc");
+    }
+
+    @Override
+    public URL getURL() {
+        try {
+            return new URL(Constant.ZAP_EXTENSIONS_PAGE);
+        } catch (MalformedURLException e) {
+            return null;
+        }
+    }
 }
