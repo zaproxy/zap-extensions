37480dcaf36255729d9098ff5f91ae0e88bebc20
kingthorin@gmail.com <kingthorin@gmail.com>
2014-05-22 13:09:43 +0000

    Issues 1169 - Support missing securityheaders.com checks (HSTS and Server)

---
+++ src/org/zaproxy/zap/extension/pscanrulesAlpha/StrictTransportSecurityScanner.java
@@ -0,0 +1,117 @@
+/*
+ * Zed Attack Proxy (ZAP) and its related class files.
+ * 
+ * ZAP is an HTTP/HTTPS proxy for assessing web application security.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); 
+ * you may not use this file except in compliance with the License. 
+ * You may obtain a copy of the License at 
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0 
+ *   
+ * Unless required by applicable law or agreed to in writing, software 
+ * distributed under the License is distributed on an "AS IS" BASIS, 
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+ * See the License for the specific language governing permissions and 
+ * limitations under the License. 
+ */
+
+package org.zaproxy.zap.extension.pscanrulesAlpha;
+
+import java.util.Vector;
+
+import net.htmlparser.jericho.Source;
+
+import org.apache.log4j.Logger;
+import org.parosproxy.paros.Constant;
+import org.parosproxy.paros.core.scanner.Alert;
+import org.parosproxy.paros.core.scanner.Category;
+import org.parosproxy.paros.network.HttpMessage;
+import org.parosproxy.paros.network.HttpHeader;
+import org.zaproxy.zap.extension.pscan.PassiveScanThread;
+import org.zaproxy.zap.extension.pscan.PluginPassiveScanner;
+
+/**
+ * Strict-Transport-Security Header Not Set passive scan rule 
+ * https://code.google.com/p/zaproxy/issues/detail?id=1169
+ * @author kingthorin+owaspzap@gmail.com
+ */
+public class StrictTransportSecurityScanner extends PluginPassiveScanner{
+
+	private static final String MESSAGE_PREFIX = "pscanalpha.stricttransportsecurity.";
+	private static final int PLUGIN_ID = 10035;
+	
+	private PassiveScanThread parent = null;
+	private static Logger logger = Logger.getLogger(StrictTransportSecurityScanner.class);
+	
+	@Override
+	public void setParent(PassiveScanThread parent) {
+		this.parent = parent;
+	}
+
+	@Override
+	public void scanHttpRequestSend(HttpMessage msg, int id) {
+		// Only checking the response for this plugin
+	}
+	
+	@Override
+	public void scanHttpResponseReceive(HttpMessage msg, int id, Source source) {
+		long start = System.currentTimeMillis();
+		if (msg.getResponseBody().length() > 0 && 
+				msg.getResponseHeader().isText() &&
+				msg.getRequestHeader().getURI().getScheme().equals(HttpHeader.HTTPS)){ //No point reporting for non-SSL resources
+			//Content available via both HTTPS and HTTP is a separate though related issue
+		Vector<String> STSOption = msg.getResponseHeader().getHeaders("Strict-Transport-Security");
+			if (STSOption == null) { // Header NOT found
+				Alert alert = new Alert(getPluginId(), Alert.RISK_LOW, Alert.WARNING, //PluginID, Risk, Reliability
+						getName()); //Name
+			    		alert.setDetail(
+			    			getDescription(), //Description
+			    			msg.getRequestHeader().getURI().toString(), //URI
+			    			"",	// Param
+			    			"", // Attack
+			    			"", // Other info
+			    			getSolution(), //Solution
+			    			getReference(), //References
+			    			"",	// Evidence
+			    			0,	// CWE Id
+			    			0,	// WASC Id
+			    			msg); //HttpMessage
+			    		parent.raiseAlert(id, alert);
+				}
+		}
+	    	if (logger.isDebugEnabled()) {
+	    		logger.debug("\tScan of record " + id + " took " + (System.currentTimeMillis() - start) + " ms");
+	    	}
+	}
+
+	@Override
+	public int getPluginId() {
+		/*
+		 * This should be unique across all active and passive rules.
+		 * The master list is http://code.google.com/p/zaproxy/source/browse/trunk/src/doc/alerts.xml
+		 */
+		return PLUGIN_ID;
+	}
+	
+	public String getName() {
+		return Constant.messages.getString(MESSAGE_PREFIX + "name");
+	}
+	
+	private String getDescription() {
+		return Constant.messages.getString(MESSAGE_PREFIX + "desc");
+	}
+
+	private String getSolution() {
+		return Constant.messages.getString(MESSAGE_PREFIX + "soln");
+	}
+
+	private String getReference() {
+		return Constant.messages.getString(MESSAGE_PREFIX + "refs");
+	}
+
+    public int getCategory() {
+        return Category.MISC;
+    }
+
+}
