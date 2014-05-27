c1f3a4e132ef616442f68e6982c7df389a83e2b4
70pointer@gmail.com <70pointer@gmail.com>
2014-05-27 23:06:19 +0000

    - Alert if the X-XSS-Protection header is not present - Alert if web browser XSS protection is disabled by the web server (in one of various configurations) - i14ize all messages, etc - Fix false negative with "X-XSS-Protection: 0; report=http://www.example.com/1/report" - Rename the scanner to more accurately reflect it's status and function - Raise the alert as a Low, rather than as an informational (since it represents a missed opportunity to implement a preventative security control, and may leave the browser increasingly exposed to XSS)

--- src/org/zaproxy/zap/extension/pscanrules/HeaderXssProtectionScanner.java
+++ src/org/zaproxy/zap/extension/pscanrules/HeaderXssProtectionScanner.java
@@ -20,12 +20,17 @@ package org.zaproxy.zap.extension.pscanrules;
 import java.util.Vector;
 
 import net.htmlparser.jericho.Source;
+
+import org.parosproxy.paros.Constant;
 import org.parosproxy.paros.core.scanner.Alert;
 import org.parosproxy.paros.network.HttpHeader;
 import org.parosproxy.paros.network.HttpMessage;
 import org.zaproxy.zap.extension.pscan.PluginPassiveScanner;
 import org.zaproxy.zap.extension.pscan.PassiveScanThread;
 
+/**
+ * a scanner to passively scan for the absence or insecure configuration of the X-XSS-Protection HTTP response header
+ */
 public class HeaderXssProtectionScanner extends PluginPassiveScanner {
 
 	private PassiveScanThread parent = null;
@@ -42,32 +47,39 @@ public class HeaderXssProtectionScanner extends PluginPassiveScanner {
 
 	@Override
 	public void scanHttpResponseReceive(HttpMessage msg, int id, Source source) {
-		if (msg.getResponseBody().length() > 0 && msg.getResponseHeader().isText()){
-			Vector<String> xssHeaderProtection = msg.getResponseHeader().getHeaders(HttpHeader.X_XSS_PROTECTION);
-			if (xssHeaderProtection != null) {
+		boolean headerPresent = false;
+		Vector<String> xssHeaderProtection = msg.getResponseHeader().getHeaders(HttpHeader.X_XSS_PROTECTION);
+		boolean possibleXSSCarrier = msg.getResponseBody().length() > 0 && msg.getResponseHeader().isText();
+		if (xssHeaderProtection != null) {
+			headerPresent = true;
+			if (possibleXSSCarrier){
 				for (String xssHeaderProtectionParam : xssHeaderProtection) {
-					if (xssHeaderProtectionParam.toLowerCase().indexOf("1") < 0) {
+					String xssHeaderProtectionParamPart1 = xssHeaderProtectionParam.split(";")[0];
+					if ( ! xssHeaderProtectionParamPart1.equals("1") ) {
 						this.raiseAlert(msg, id, xssHeaderProtectionParam);
 					}
 				}
-			}
+			} 
+		}
+		if ( (! headerPresent) && possibleXSSCarrier) { 
+			//no header, so raise an alert
+			this.raiseAlert(msg, id, null);
 		}
 	}
 	
 	private void raiseAlert(HttpMessage msg, int id, String xssHeaderProtection) {
-		Alert alert = new Alert(getPluginId(), Alert.RISK_INFO, Alert.WARNING, 
-		    	getName());
-		    	alert.setDetail(
-		    			"The x-xss-protection header has been disabled by the web application", 
+		Alert alert = new Alert(getPluginId(), Alert.RISK_LOW, Alert.WARNING,  getName());
+		alert.setDetail(
+					Constant.messages.getString("pscanrules.xss-protection.desc"), 
 		    	    msg.getRequestHeader().getURI().toString(),
-		    	    xssHeaderProtection,
-		    	    "", 
-		    	    "",
-		    	    "Enable the IE's XSS filter. If it must be disabled for any reasons, ensure that the application is properly protected against XSS vulnerability", 
-		            "https://www.owasp.org/index.php/XSS_(Cross_Site_Scripting)_Prevention_Cheat_Sheet", 
-		            "", // No evidence
-		            0,	// TODO CWE Id
-		            0,	// TODO WASC Id
+		    	    "",  					//parameter
+		    	    "", 					//attack
+		    	    Constant.messages.getString("pscanrules.xss-protection.extrainfo"),		//other info
+		    	    Constant.messages.getString("pscanrules.xss-protection.soln"),			//solution 
+		    	    Constant.messages.getString("pscanrules.xss-protection.refs"),			//refs 
+		            (xssHeaderProtection!= null?HttpHeader.X_XSS_PROTECTION+": "+xssHeaderProtection:""), 	//evidence, if any
+		            933, //CWE-933: OWASP Top Ten 2013 Category A5 - Security Misconfiguration
+		            14,  //WASC-14: Server Misconfiguration
 		            msg);
 	
     	parent.raiseAlert(id, alert);
@@ -80,7 +92,7 @@ public class HeaderXssProtectionScanner extends PluginPassiveScanner {
 	
 	@Override
 	public String getName() {
-		return "IE8's XSS protection filter not disabled";
+		return Constant.messages.getString("pscanrules.xss-protection.name");
 	}
 	
 }
