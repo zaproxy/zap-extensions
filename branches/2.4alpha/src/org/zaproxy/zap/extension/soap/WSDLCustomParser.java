5cc7a06a4e8ade18165f90262c6a577139b1ccc5
albertov91@gmail.com <albertov91@gmail.com@1e26c7e7-97f5-7f45-da66-271314645f9b>
2014-07-12 10:53:49 +0000

    Minor fixes.

--- branches/2.4alpha/src/org/zaproxy/zap/extension/soap/WSDLCustomParser.java
+++ branches/2.4alpha/src/org/zaproxy/zap/extension/soap/WSDLCustomParser.java
@@ -114,7 +114,11 @@ public class WSDLCustomParser {
 		try {
 			if (View.isInitialised()) {
 				// Switch to the output panel, if in GUI mode
-				View.getSingleton().getOutputPanel().setTabFocus();
+				try{
+					View.getSingleton().getOutputPanel().setTabFocus();
+				}catch(Exception e){
+					log.debug("Could not set tab focus on Output Panel.");
+				}
 			}
 			/* Sends a request to retrieve remote WSDL file's content. */
 			HttpMessage httpRequest = new HttpMessage(new URI(url, false));
@@ -243,55 +247,53 @@ public class WSDLCustomParser {
 		HashMap<String, String> formParams = new HashMap<String, String>();
     	for(Part part : requestParts){
     		try{
-	    		if (part.getName().equals("parameters")){
-	    			final String elementName = part.getElement().getName();
-	        		ComplexType ct = (ComplexType) part.getElement().getEmbeddedType();
-	        		/* Handles when ComplexType is not embedded but referenced by 'type'. */
-	        		if (ct == null){
-	        			Element element = part.getElement();
-	        			Schema currentSchema = element.getSchema();
-	        			ct = (ComplexType) currentSchema.getType(element.getType());
-	        		}			    	        			
-	        		for (Element e : ct.getSequence().getElements()) {
-	        			String paramName = e.getName();
-	        			if(paramName != null) paramName = paramName.trim();
-	        			else{
-	        				/* Handles simple types. */
-	        				Schema currentSchema = e.getSchema();
-	        				SimpleType simpleType = (SimpleType) currentSchema.getType(e.getType());
-	        				paramName = simpleType.getName();
-	        				if (paramName != null) paramName = paramName.trim();
-	        				else{
-	        					/* Handles simple types restrictions. */
-	        					paramName = simpleType.getRestriction().getBase().getQualifiedName().trim();
-	        				}
-	        						
-	        			}
-	        			log.info("Detected parameter name: "+paramName);
-	        			String paramType = e.getType().getQualifiedName().trim();
-	        			if(paramType.contains(":")){
-	        				String[] stringParts = paramType.split(":");
-	        				paramType = stringParts[stringParts.length-1];
-	        			}
-	        			/* Parameter value depends on parameter type. */
-	        			if(paramType.equals("string")){
-		        			formParams.put("xpath:/"+elementName.trim()+"/"+paramName, "paramValue");
-		        		}else if(paramType.equals("int") || paramType.equals("double") ||
-		        				paramType.equals("long")){
-		        			formParams.put("xpath:/"+elementName.trim()+"/"+paramName, "0");
-		        		}else if(paramType.equals("date")){
-		        			Date date = new Date();
-		        			SimpleDateFormat dt1 = new SimpleDateFormat("CCYY-MM-DD");
-		        			String dateS = dt1.format(date);
-		        			formParams.put("xpath:/"+elementName.trim()+"/"+paramName, dateS);
-		        		}else if(paramType.equals("dateTime")){
-		        			Date date = new Date();
-		        			SimpleDateFormat dt1 = new SimpleDateFormat("CCYY-MM-DDThh:mm:ssZ");
-		        			String dateS = dt1.format(date);
-		        			formParams.put("xpath:/"+elementName.trim()+"/"+paramName, dateS);
-		        		}
+    			final String elementName = part.getElement().getName();
+        		ComplexType ct = (ComplexType) part.getElement().getEmbeddedType();
+        		/* Handles when ComplexType is not embedded but referenced by 'type'. */
+        		if (ct == null){
+        			Element element = part.getElement();
+        			Schema currentSchema = element.getSchema();
+        			ct = (ComplexType) currentSchema.getType(element.getType());
+        		}			    	        			
+        		for (Element e : ct.getSequence().getElements()) {
+        			String paramName = e.getName();
+        			if(paramName != null) paramName = paramName.trim();
+        			else{
+        				/* Handles simple types. */
+        				Schema currentSchema = e.getSchema();
+        				SimpleType simpleType = (SimpleType) currentSchema.getType(e.getType());
+        				paramName = simpleType.getName();
+        				if (paramName != null) paramName = paramName.trim();
+        				else{
+        					/* Handles simple types restrictions. */
+        					paramName = simpleType.getRestriction().getBase().getQualifiedName().trim();
+        				}
+        						
+        			}
+        			log.info("Detected parameter name: "+paramName);
+        			String paramType = e.getType().getQualifiedName().trim();
+        			if(paramType.contains(":")){
+        				String[] stringParts = paramType.split(":");
+        				paramType = stringParts[stringParts.length-1];
+        			}
+        			/* Parameter value depends on parameter type. */
+        			if(paramType.equals("string")){
+	        			formParams.put("xpath:/"+elementName.trim()+"/"+paramName, "paramValue");
+	        		}else if(paramType.equals("int") || paramType.equals("double") ||
+	        				paramType.equals("long")){
+	        			formParams.put("xpath:/"+elementName.trim()+"/"+paramName, "0");
+	        		}else if(paramType.equals("date")){
+	        			Date date = new Date();
+	        			SimpleDateFormat dt1 = new SimpleDateFormat("CCYY-MM-DD");
+	        			String dateS = dt1.format(date);
+	        			formParams.put("xpath:/"+elementName.trim()+"/"+paramName, dateS);
+	        		}else if(paramType.equals("dateTime")){
+	        			Date date = new Date();
+	        			SimpleDateFormat dt1 = new SimpleDateFormat("CCYY-MM-DDThh:mm:ssZ");
+	        			String dateS = dt1.format(date);
+	        			formParams.put("xpath:/"+elementName.trim()+"/"+paramName, dateS);
 	        		}
-	    		}
+        		}
     		}catch(Exception e){
         		log.warn("There was an error when trying to parse part "+part.getName()+" from WSDL file.");
         	}
@@ -367,6 +369,7 @@ public class WSDLCustomParser {
 				Model.getSingleton().getOptionsParam().getConnectionParam(),
 				true,
 				HttpSender.MANUAL_REQUEST_INITIATOR);
+        /* Send request. */
         try {
 			sender.sendAndReceive(httpRequest, true);
 		} catch (IOException e) {
