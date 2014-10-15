2b0e70a9204e75d9eeb042dceb2341f3d99486ac
THC202@gmail.com <thc202@users.noreply.github.com>
2014-10-15 14:02:01 +0000

    Issue 232 - Feature Request: String Highlighter
     - Restored the highlighting functionality.
     - Updated the "changes" and version of "Highlighter" add-on.

--- src/org/zaproxy/zap/extension/highlighter/HighlightEntryLineUi.java
+++ src/org/zaproxy/zap/extension/highlighter/HighlightEntryLineUi.java
@@ -14,6 +14,7 @@ import javax.swing.JPanel;
 import javax.swing.JTextField;
 
 import org.parosproxy.paros.extension.AbstractPanel;
+import org.zaproxy.zap.view.HighlightSearchEntry;
 
 /*
  * A panel which specifies all values of an HighlightEntry in a JPanel,
