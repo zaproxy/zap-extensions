e106a587447d5edab618cff368cca9d281b4f3af
psiinon@gmail.com <psiinon@gmail.com@1e26c7e7-97f5-7f45-da66-271314645f9b>
2014-11-03 14:37:30 +0000

    Latest language files from Crowdin

--- branches/alpha/src/org/zaproxy/zap/extension/ascanrulesAlpha/resources/help_de_DE/helpset_de_DE.hs
+++ branches/alpha/src/org/zaproxy/zap/extension/ascanrulesAlpha/resources/help_de_DE/helpset_de_DE.hs
@@ -1,7 +1,7 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <!DOCTYPE helpset PUBLIC "-//Sun Microsystems Inc.//DTD JavaHelp HelpSet Version 2.0//EN" "http://java.sun.com/products/javahelp/helpset_2_0.dtd">
 <helpset version="2.0" xml:lang="de-DE">
-  <title>Active Scan Rules - Blpha | ZAP Extension</title>
+  <title>Active Scan Rules - Alpha | ZAP Extension</title>
 
   <maps>
      <homeID>top</homeID>
