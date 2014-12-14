6f5fd7a31a4d852585fb317b8c50e03059eabeb4
maqianlie <maqianlie@1e26c7e7-97f5-7f45-da66-271314645f9b>
2014-12-14 05:23:15 +0000

    add OptionDialog.java for class OptionDialog.

---
+++ branches/alpha/src/org/zaproxy/zap/extension/advreport/OptionDialog.java
@@ -0,0 +1,16 @@
+package org.parosproxy.paros.extension.advreport;
+
+import javax.swing.JTabbedPane;
+
+import org.parosproxy.paros.view.AbstractFrame;
+
+public class OptionDialog extends AbstractFrame{
+	
+	public OptionDialog( ScopePanel scopeponel, AdvancedPanel advancedpanel){
+		JTabbedPane mainpane = new JTabbedPane();
+        mainpane.add("Scope", scopeponel );
+        mainpane.add("Advanced", advancedpanel );
+        this.add(mainpane);
+        this.pack();
+	}
+}
