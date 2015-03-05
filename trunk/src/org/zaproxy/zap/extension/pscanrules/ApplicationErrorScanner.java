6f9372eb37800940a38907f2da3da788e1214f56
70pointer@gmail.com <70pointer@gmail.com@1e26c7e7-97f5-7f45-da66-271314645f9b>
2015-03-05 16:02:24 +0000

    Update from deprecated Alert.WARNING

--- trunk/src/org/zaproxy/zap/extension/pscanrules/ApplicationErrorScanner.java
+++ trunk/src/org/zaproxy/zap/extension/pscanrules/ApplicationErrorScanner.java
@@ -147,7 +147,7 @@ public class ApplicationErrorScanner extends PluginPassiveScanner {
         // Raise an alert according to Passive Scan Rule model
         // description, uri, param, attack, otherInfo, 
         // solution, reference, evidence, cweId, wascId, msg
-        Alert alert = new Alert(getPluginId(), getRisk(), Alert.WARNING, getName());
+        Alert alert = new Alert(getPluginId(), getRisk(), Alert.CONFIRMED, getName());
         alert.setDetail(
                 getDescription(),
                 msg.getRequestHeader().getURI().toString(),
