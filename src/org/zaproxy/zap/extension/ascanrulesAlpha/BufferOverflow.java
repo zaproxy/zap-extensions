34f146db2d2edc4bcd945aaff454651589cc84d9
msrader@gmail.com <msrader@gmail.com@1e26c7e7-97f5-7f45-da66-271314645f9b>
2015-04-19 23:38:05 +0000

    Updating license to make it more permissive as GPL

--- src/org/zaproxy/zap/extension/ascanrulesAlpha/BufferOverflow.java
+++ src/org/zaproxy/zap/extension/ascanrulesAlpha/BufferOverflow.java
@@ -15,6 +15,30 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
+/**
+ * Buffer Overflow an active scan rule
+ * Copyright (C) 2015 Institute for Defense Analyses
+ * @author Mark Rader based upon the example active scanner by psiinon
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE
+ */
+
 package org.zaproxy.zap.extension.ascanrulesAlpha;
 
 
@@ -30,12 +54,7 @@ import org.zaproxy.zap.model.Vulnerabilities;
 import org.zaproxy.zap.model.Vulnerability;
 import org.zaproxy.zap.network.HttpResponseBody;
 
-/**
- * A Buffer Overflow  scan rule, for more details see 
- * Copyright (C) 2014-2015 Institute for Defense Analyses
- * @author Mark Rader based upon the example active scanner by psiinon
- * 
- */
+
 public class BufferOverflow extends AbstractAppParamPlugin  {
 
 	// wasc_7 is a buffer overflow ;)
