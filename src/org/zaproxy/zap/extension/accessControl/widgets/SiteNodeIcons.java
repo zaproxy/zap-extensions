79bfc056d48021873581029a2ae66792f3fd2b69
thc202 <thc202@gmail.com>
2017-10-06 21:26:16 +0100

    accessControl: correct resource loading
    Change SiteNodeIcons and SiteTreeNodeCellRenderer (the latter class is
    currently unused) to use the classes themselves to load the resources,
    which ensures it's used the correct class loader to load the resource.
    Bump version and update changes in ZapAddOn.xml file.
    
    Part of zaproxy/zaproxy#2602 - Java 9
    Reported in zaproxy/zaproxy#3934.

--- src/org/zaproxy/zap/extension/accessControl/widgets/SiteNodeIcons.java
+++ src/org/zaproxy/zap/extension/accessControl/widgets/SiteNodeIcons.java
@@ -20,31 +20,28 @@
 package org.zaproxy.zap.extension.accessControl.widgets;
 
 import javax.swing.ImageIcon;
-import javax.swing.tree.DefaultTreeCellRenderer;
 
 public final class SiteNodeIcons {
 	public static final ImageIcon ROOT_ICON = new ImageIcon(
-			DefaultTreeCellRenderer.class.getResource("/resource/icon/16/094.png"));
+			SiteNodeIcons.class.getResource("/resource/icon/16/094.png"));
 	public static final ImageIcon LEAF_ICON = new ImageIcon(
-			DefaultTreeCellRenderer.class.getResource("/resource/icon/fugue/document.png"));
+			SiteNodeIcons.class.getResource("/resource/icon/fugue/document.png"));
 	public static final ImageIcon FOLDER_OPEN_ICON = new ImageIcon(
-			DefaultTreeCellRenderer.class.getResource("/resource/icon/fugue/folder-horizontal-open.png"));
+			SiteNodeIcons.class.getResource("/resource/icon/fugue/folder-horizontal-open.png"));
 	public static final ImageIcon FOLDER_CLOSED_ICON = new ImageIcon(
-			DefaultTreeCellRenderer.class.getResource("/resource/icon/fugue/folder-horizontal.png"));
+			SiteNodeIcons.class.getResource("/resource/icon/fugue/folder-horizontal.png"));
 	public static final ImageIcon LEAF_ICON_CHECK = new ImageIcon(
-			DefaultTreeCellRenderer.class.getResource("/resource/icon/fugue/document-check.png"));
+			SiteNodeIcons.class.getResource("/resource/icon/fugue/document-check.png"));
 	public static final ImageIcon FOLDER_OPEN_ICON_CHECK = new ImageIcon(
-			DefaultTreeCellRenderer.class
-					.getResource("/resource/icon/fugue/folder-horizontal-open-check.png"));
+			SiteNodeIcons.class.getResource("/resource/icon/fugue/folder-horizontal-open-check.png"));
 	public static final ImageIcon FOLDER_CLOSED_ICON_CHECK = new ImageIcon(
-			DefaultTreeCellRenderer.class.getResource("/resource/icon/fugue/folder-horizontal-check.png"));
+			SiteNodeIcons.class.getResource("/resource/icon/fugue/folder-horizontal-check.png"));
 	public static final ImageIcon LEAF_ICON_CROSS = new ImageIcon(
-			DefaultTreeCellRenderer.class.getResource("/resource/icon/fugue/document-cross.png"));
+			SiteNodeIcons.class.getResource("/resource/icon/fugue/document-cross.png"));
 	public static final ImageIcon FOLDER_OPEN_ICON_CROSS = new ImageIcon(
-			DefaultTreeCellRenderer.class
-					.getResource("/resource/icon/fugue/folder-horizontal-open-cross.png"));
+			SiteNodeIcons.class.getResource("/resource/icon/fugue/folder-horizontal-open-cross.png"));
 	public static final ImageIcon FOLDER_CLOSED_ICON_CROSS = new ImageIcon(
-			DefaultTreeCellRenderer.class.getResource("/resource/icon/fugue/folder-horizontal-cross.png"));
+			SiteNodeIcons.class.getResource("/resource/icon/fugue/folder-horizontal-cross.png"));
 
 	private SiteNodeIcons() {
 		// Utility class
