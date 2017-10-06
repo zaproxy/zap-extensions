8bfac4bacbc400db334afc2613985c2086ab5373
thc202 <thc202@gmail.com>
2017-10-06 23:55:29 +0100

    Remove usage of java.awt.Event
    Replace usage of java.awt.Event with KeyEvent, the former is deprecated
    in Java 9, also, update to recommended (non-deprecated) masks (e.g.
    SHIFT_DOWN_MASK instead of SHIFT_MASK).
    Update changes (and bump version, where required) in ZapAddOn.xml files
    of the corresponding add-ons.
    
    Part of #2602 - Java 9

--- src/org/zaproxy/zap/extension/websocket/ui/WebSocketPanel.java
+++ src/org/zaproxy/zap/extension/websocket/ui/WebSocketPanel.java
@@ -19,7 +19,6 @@ package org.zaproxy.zap.extension.websocket.ui;
 
 import java.awt.Component;
 import java.awt.Dimension;
-import java.awt.Event;
 import java.awt.EventQueue;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
@@ -173,7 +172,7 @@ public class WebSocketPanel extends AbstractPanel implements WebSocketObserver {
 	 */
 	private void initializePanel() {
 		setName(Constant.messages.getString("websocket.panel.title"));
-		setDefaultAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_W, Toolkit.getDefaultToolkit().getMenuShortcutKeyMask() | Event.SHIFT_MASK, false));
+		setDefaultAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_W, Toolkit.getDefaultToolkit().getMenuShortcutKeyMask() | KeyEvent.SHIFT_DOWN_MASK, false));
 		setMnemonic(Constant.messages.getChar("websocket.panel.mnemonic"));
 
 		
