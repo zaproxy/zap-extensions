805b5883c906ae57bab25e09141e3a14d83bdc3c
zapbot <zapbot@zaproxy.org>
2018-01-24 00:07:18 +0000

    Latest files from Crowdin

--- src/org/zaproxy/zap/extension/jxbrowser/resources/help_id_ID/helpset_id_ID.hs
+++ src/org/zaproxy/zap/extension/jxbrowser/resources/help_id_ID/helpset_id_ID.hs
@@ -1,7 +1,7 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <!DOCTYPE helpset PUBLIC "-//Sun Microsystems Inc.//DTD JavaHelp HelpSet Version 2.0//EN" "http://java.sun.com/products/javahelp/helpset_2_0.dtd">
 <helpset version="2.0" xml:lang="id-ID">
-  <title>JxBrowser | ZAP Extension</title>
+  <title>JxBrowser | ZAP ekstensi</title>
 
   <maps>
      <homeID>top</homeID>
@@ -24,7 +24,7 @@
 
   <view>
     <name>Search</name>
-    <label>Mencari</label>
+    <label>Telusuri</label>
     <type>javax.help.SearchView</type>
     <data engine="com.sun.java.help.search.DefaultSearchEngine">
       JavaHelpSearch
