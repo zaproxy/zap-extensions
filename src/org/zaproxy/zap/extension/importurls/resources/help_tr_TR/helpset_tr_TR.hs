c15138128896de8354002bed536c60277a4d1788
zapbot <zapbot@zaproxy.org>
2018-01-27 00:06:35 +0000

    Latest files from Crowdin

--- src/org/zaproxy/zap/extension/importurls/resources/help_tr_TR/helpset_tr_TR.hs
+++ src/org/zaproxy/zap/extension/importurls/resources/help_tr_TR/helpset_tr_TR.hs
@@ -1,7 +1,7 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <!DOCTYPE helpset PUBLIC "-//Sun Microsystems Inc.//DTD JavaHelp HelpSet Version 2.0//EN" "http://java.sun.com/products/javahelp/helpset_2_0.dtd">
 <helpset version="2.0" xml:lang="tr-TR">
-  <title>Import Urls | ZAP Extension</title>
+  <title>URL'leri İçe Aktar| ZAP Uzantısı</title>
 
   <maps>
      <homeID>top</homeID>
