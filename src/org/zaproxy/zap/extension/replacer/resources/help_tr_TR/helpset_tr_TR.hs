f375c76861bbc37dc7b4cbc7c6b931d4f2967c10
zapbot <zapbot@zaproxy.org>
2018-01-29 00:06:19 +0000

    Latest files from Crowdin

--- src/org/zaproxy/zap/extension/replacer/resources/help_tr_TR/helpset_tr_TR.hs
+++ src/org/zaproxy/zap/extension/replacer/resources/help_tr_TR/helpset_tr_TR.hs
@@ -1,7 +1,7 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <!DOCTYPE helpset PUBLIC "-//Sun Microsystems Inc.//DTD JavaHelp HelpSet Version 2.0//EN" "http://java.sun.com/products/javahelp/helpset_2_0.dtd">
 <helpset version="2.0" xml:lang="tr-TR">
-  <title>Replacer | ZAP Extension</title>
+  <title>Yer Alıcı | ZAP Uzantısı</title>
 
   <maps>
      <homeID>top</homeID>
