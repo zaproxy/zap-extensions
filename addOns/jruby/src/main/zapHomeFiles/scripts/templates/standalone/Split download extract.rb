847f507f102e65f563f98106eead26bf5d598e1b
Fabian Franz BSc <fabianfrz@users.noreply.github.com>
2019-08-03 11:10:55 +0200

    Add script to concatenate split downloads

---
+++ addOns/jruby/src/main/zapHomeFiles/scripts/templates/standalone/Split download extract.rb
@@ -0,0 +1,24 @@
+require 'java'
+
+# Adjust the script to get join a file download which is split over several requests.
+# This is useful if you have a JavaScript file download that downloads chunks of the file
+# to reduce the risk of a broken connection.
+
+extHist = org.parosproxy.paros.control.Control.getSingleton().getExtensionLoader().getExtension("ExtensionHistory")
+raw_data = []
+if (extHist != nil)
+  
+  File.open 'YOUR FILE PATH', 'wb' do |f|
+    extHist.getSelectedHistoryReferences.each do |hr|
+      content_type = hr.getHttpMessage.getResponseHeader.getHeader("Content-Type")
+      url = url = hr.getHttpMessage.getRequestHeader.getURI.toString
+      # additional filter to jump over files that are not desired - for example if you have a application
+      # that is polling data open in background so and you just do not want to manually select them in the history.
+      # if you do not want the filter, commment out the next line and the corresponding end
+      if (content_type.include? 'octet-stream') && (url.include? 'www.example.com')
+        payload = hr.getHttpMessage.getResponseBody.getBytes
+        f.write payload if payload
+      end
+    end
+  end
+end
