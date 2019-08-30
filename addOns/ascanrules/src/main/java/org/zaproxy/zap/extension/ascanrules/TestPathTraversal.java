4dc68e6c6c622dd767bff71d79be531b69cd0da1
KSASAN <preetkaran20@gmail.com>
2019-08-30 23:34:29 +0530

    Updating code as per review comments

--- addOns/ascanrules/src/main/java/org/zaproxy/zap/extension/ascanrules/TestPathTraversal.java
+++ addOns/ascanrules/src/main/java/org/zaproxy/zap/extension/ascanrules/TestPathTraversal.java
@@ -252,6 +252,8 @@ public class TestPathTraversal extends AbstractAppParamPlugin {
             int winCount = 0;
             int dirCount = 0;
             int localTraversalLength = 0;
+            int index = -1;
+            String extension = null;
 
             // DEBUG only
             if (log.isDebugEnabled()) {
@@ -290,6 +292,12 @@ public class TestPathTraversal extends AbstractAppParamPlugin {
                     dirCount = LOCAL_DIR_TARGETS.length;
                     localTraversalLength = 4;
                     includeNullByteInjectionPayload = true;
+                    if(value != null) {
+                    	index = value.lastIndexOf(".");
+                        if (index != -1) {
+                            extension = value.substring(index);
+                        }
+                    }
                     break;
 
                 default:
@@ -307,14 +315,6 @@ public class TestPathTraversal extends AbstractAppParamPlugin {
                                 + "] for Path Traversal to local files");
             }
 
-            int index = -1;
-            String extension = null;
-            if(value != null) {
-            	index = value.lastIndexOf(".");
-                if (index != -1) {
-                    extension = value.substring(index);
-                }
-            }
             // Check 1: Start detection for Windows patterns
             // note that depending on the AttackLevel, the number of prefixes that we will try
             // changes.
