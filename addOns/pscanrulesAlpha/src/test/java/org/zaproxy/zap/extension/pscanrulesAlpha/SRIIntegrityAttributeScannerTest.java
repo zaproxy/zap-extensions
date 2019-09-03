28a64fbafe8dcceb2469ab95b46ded907dfbb617
Cotonne <cotonne@github.com>
2019-09-03 21:13:38 +0200

    SRI - missing integrity attribute  - No alert when integrity attribute is present in script tag

--- addOns/pscanrulesAlpha/src/test/java/org/zaproxy/zap/extension/pscanrulesAlpha/SRIIntegrityAttributeScannerTest.java
+++ addOns/pscanrulesAlpha/src/test/java/org/zaproxy/zap/extension/pscanrulesAlpha/SRIIntegrityAttributeScannerTest.java
@@ -2,17 +2,14 @@ package org.zaproxy.zap.extension.pscanrulesAlpha;
 
 import org.junit.Test;
 import org.parosproxy.paros.network.HttpMessage;
-import org.parosproxy.paros.network.HttpStatusCode;
 
 import static org.hamcrest.Matchers.equalTo;
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertThat;
 
 public class SRIIntegrityAttributeScannerTest
     extends PassiveScannerTest<SRIIntegrityAttributeScanner> {
 
   // Test cases
-  // With integrity attribute in script
-  // With integrity attribute in link
   // Without attribute but in the current domain => No alert
   // Without attribute but in a different domain => Alert
   // script in body?
@@ -26,6 +23,23 @@ public class SRIIntegrityAttributeScannerTest
         "<html><head><link rel=\"stylesheet\" href=\"https://site53.example.net/style.css\"\n"
             + "      integrity=\"sha384-+/M6kredJcxdsqkczBUjMLvqyHb1K/JThDXWsBVxMEeZHEaMKEOEct339VItX1zB\"\n"
             + "      crossorigin=\"anonymous\"></head><body></body></html>");
+
+    // When the page is scanned
+    rule.scanHttpResponseReceive(msg, -1, createSource(msg));
+
+    // Then no alert should be raised
+    assertThat(alertsRaised.size(), equalTo(0));
+  }
+
+  @Test
+  public void shouldNotRaiseAlertGivenIntegrityAttributeIsPresentInScriptTag() {
+    // Given a HTML page with link tag containing an integrity attribute
+    HttpMessage msg = new HttpMessage();
+    // from https://www.w3.org/TR/SRI/#use-casesexamples
+    msg.setResponseBody(
+        "<html><head><script src=\"https://analytics-r-us.example.com/v1.0/include.js\"\n"
+            + "        integrity=\"sha384-MBO5IDfYaE6c6Aao94oZrIOiC6CGiSN2n4QUbHNPhzk5Xhm0djZLQqTpL0HzTUxk\"\n"
+            + "        crossorigin=\"anonymous\"></script></head><body></body></html>");
 
     // When the page is scanned
     rule.scanHttpResponseReceive(msg, -1, createSource(msg));
