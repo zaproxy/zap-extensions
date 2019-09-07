7963ea7b6c0597073735d43b63d38282ef28cc4f
KSASAN <preetkaran20@gmail.com>
2019-09-07 23:51:12 +0530

    Adding Null Byte code to CrossSiteScripts

--- addOns/ascanrules/src/main/java/org/zaproxy/zap/extension/ascanrules/TestCrossSiteScriptV2.java
+++ addOns/ascanrules/src/main/java/org/zaproxy/zap/extension/ascanrules/TestCrossSiteScriptV2.java
@@ -163,9 +163,10 @@ public class TestCrossSiteScriptV2 extends AbstractAppParamPlugin {
             return null;
         }
         if (isNullByteSpecialHandling) {
-            // Removing Null Bytes as parser tries to find the enclosing tag on attack vector (eg
+        	//Special handling for case where Attack Vector is reflected outside of html tag. 
+            //Removing Null Byte as parser tries to find the enclosing tag on attack vector (eg
             // \0<script>alert(1);</script>) starting from first character
-            // and as null byte is not starting any tag or part of script tag so fails.
+            // and as null byte is not starting any tag and there is no enclosing tag for null byte so parent context is null.
             attack = attack.replaceFirst(NULL_BYTE_CHARACTER, "");
         }
         HtmlContextAnalyser hca = new HtmlContextAnalyser(msg2);
