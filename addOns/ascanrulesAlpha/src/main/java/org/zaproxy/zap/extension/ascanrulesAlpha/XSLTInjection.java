6257ca888e084e6068815de067effb1b26cc007c
ZainabAlShowely <zalsh070@uottawa.ca>
2019-09-17 11:11:16 -0400

    Corrects other issues noted and updates documentation
    Signed-off-by: ZainabAlShowely <zalsh070@uottawa.ca>

--- addOns/ascanrulesAlpha/src/main/java/org/zaproxy/zap/extension/ascanrulesAlpha/XSLTInjection.java
+++ addOns/ascanrulesAlpha/src/main/java/org/zaproxy/zap/extension/ascanrulesAlpha/XSLTInjection.java
@@ -20,7 +20,8 @@
 package org.zaproxy.zap.extension.ascanrulesAlpha;
 
 import java.io.IOException;
-import java.util.HashMap;
+import java.util.Arrays;
+import java.util.EnumMap;
 import java.util.Map;
 import org.apache.commons.httpclient.URIException;
 import org.apache.log4j.Logger;
@@ -39,57 +40,67 @@ import org.parosproxy.paros.network.HttpMessage;
 public class XSLTInjection extends AbstractAppParamPlugin {
     private static final String MESSAGE_PREFIX = "ascanalpha.xsltinjection.";
 
-    private static final String[] errorCausingPayloads = {"<"};
-
-    private static final String[] evidenceError = {
-        "compilation error", "XSLT compile error", "SAXParseException"
-    };
-
-    private static final String[] vendorReturningPayloads = {
-        "<xsl:value-of select=\"system-property(\'xsl:vendor\')\"/>",
-        "system-property(\'xsl:vendor\')/>",
-        "\"/><xsl:value-of select=\"system-property(\'xsl:vendor\')\"/><!--",
-        "<xsl:value-of select=\"system-property(\'xsl:vendor\')\"/><!--"
-    };
-
-    private static final String[] evidenceVendors = {
-        "libxslt", "Microsoft", "Saxonica", "Apache", "Xalan", "SAXON", "Transformiix"
-    };
-
-    private static final String[] evidencePortScanning = {
-        "failed to open stream",
-        "Invalid Http response",
-        "Connection Refused",
-        "No connection could be made because the target machine actively refused it",
-        "Can not load requested doc"
-    };
-
-    private static final String[] cmdExecPayloads = {
-        "<xsl:variable name=\"rtobject\" select=\"runtime:getRuntime()\"/>\n"
-                + "<xsl:variable name=\"process\" select=\"runtime:exec($rtobject,'erroneous_command')\"/>\n"
-                + "<xsl:variable name=\"waiting\" select=\"process:waitFor($process)\"/>\n"
-                + "<xsl:value-of select=\"$process\"/>",
-        "<xsl:value-of select=\"php:function('exec','erroneous_command 2>&amp;1')\"/>"
-    };
-
-    private static final String[] evidenceCmdExec = {
-        "Cannot run program", "erroneous_command: not found",
-    };
-
     private enum XSLTInjectionType {
-        ERROR(errorCausingPayloads, evidenceError, "error"),
-        VENDOR(vendorReturningPayloads, evidenceVendors, "vendor"),
-        PORTSCAN(new String[] {}, evidencePortScanning, "portscan"),
-        COMMAND_EXEC(cmdExecPayloads, evidenceCmdExec, "command");
+        ERROR(
+                new String[] {"<"},
+                new String[] {"compilation error", "XSLT compile error", "SAXParseException"},
+                "error"),
+        VENDOR(
+                new String[] {
+                    "<xsl:value-of select=\"system-property(\'xsl:vendor\')\"/>",
+                    "system-property(\'xsl:vendor\')/>",
+                    "\"/><xsl:value-of select=\"system-property(\'xsl:vendor\')\"/><!--",
+                    "<xsl:value-of select=\"system-property(\'xsl:vendor\')\"/><!--"
+                },
+                new String[] {
+                    "libxslt", "Microsoft", "Saxonica", "Apache", "Xalan", "SAXON", "Transformiix"
+                },
+                "vendor"),
+        PORTSCAN(
+                new String[] {},
+                new String[] {
+                    "failed to open stream",
+                    "Invalid Http response",
+                    "Connection Refused",
+                    "No connection could be made because the target machine actively refused it",
+                    "Can not load requested doc"
+                },
+                "portscan"),
+        COMMAND_EXEC(
+                new String[] {
+                    "<xsl:variable name=\"rtobject\" select=\"runtime:getRuntime()\"/>\n"
+                            + "<xsl:variable name=\"process\" select=\"runtime:exec($rtobject,'erroneous_command')\"/>\n"
+                            + "<xsl:variable name=\"waiting\" select=\"process:waitFor($process)\"/>\n"
+                            + "<xsl:value-of select=\"$process\"/>",
+                    "<xsl:value-of select=\"php:function('exec','erroneous_command 2>&amp;1')\"/>"
+                },
+                new String[] {
+                    "Cannot run program", "erroneous_command: not found",
+                },
+                "command");
 
         private String[] payloads;
-        private String[] responses;
+        private String[] evidences;
         private String resourceIdentifier;
 
         XSLTInjectionType(String[] payloads, String[] responses, String resourceIdentifier) {
             this.payloads = payloads;
-            this.responses = responses;
+            this.evidences = responses;
             this.resourceIdentifier = resourceIdentifier;
+        }
+
+        private String[] getPayloads(HttpMessage msg) {
+            return this.name().equals("PORTSCAN")
+                    ? new String[] {getXslForPortScan(msg)}
+                    : payloads;
+        }
+
+        private String[] getEvidences() {
+            return evidences;
+        }
+
+        private String getResourceIdentifier() {
+            return resourceIdentifier;
         }
     }
 
@@ -98,19 +109,18 @@ public class XSLTInjection extends AbstractAppParamPlugin {
     // used to check against the attack strength
     private int requestsSent = 0;
     // map setting the limits associated with each attack strength
-    private static Map<AttackStrength, Integer> strengthToRequestCountMap = new HashMap<>();
+    private static Map<AttackStrength, Integer> strengthToRequestCountMap =
+            new EnumMap<>(AttackStrength.class);
 
     static {
         strengthToRequestCountMap.put(AttackStrength.LOW, 6);
         strengthToRequestCountMap.put(AttackStrength.MEDIUM, 12);
         strengthToRequestCountMap.put(AttackStrength.HIGH, 24);
+        strengthToRequestCountMap.put(AttackStrength.INSANE, 24);
     }
 
     @Override
     public void scan(HttpMessage msg, String param, String value) {
-        // sets the payloads that recquire an instance to be determined
-        XSLTInjectionType.PORTSCAN.payloads = new String[] {getXslForPortScan()};
-
         // goes through all checks and stops if it finds a possible
         // injection
         for (XSLTInjectionType check : XSLTInjectionType.values()) {
@@ -121,7 +131,7 @@ public class XSLTInjection extends AbstractAppParamPlugin {
     }
 
     private Boolean tryInjection(HttpMessage msg, String param, XSLTInjectionType checkType) {
-        for (String payload : checkType.payloads) {
+        for (String payload : checkType.getPayloads(msg)) {
             try {
                 if (isStop() || requestsLimitReached()) { // stop before sending request
                     if (LOG.isDebugEnabled()) {
@@ -129,16 +139,19 @@ public class XSLTInjection extends AbstractAppParamPlugin {
                     }
                     return false;
                 }
+
+                if (Arrays.stream(checkType.getEvidences())
+                        .anyMatch(getBaseMsg().getResponseBody().toString()::contains)) {
+                    continue; // skip as the original contains a suspicious response
+                }
+
                 msg = sendRequest(msg, param, payload);
 
-                for (String response : checkType.responses) {
-                    if (getBaseMsg().getResponseBody().toString().contains(response)) {
-                        continue; // skip as the original contains the suspicious response
-                    }
-
+                for (String response : checkType.getEvidences()) {
                     if (msg.getResponseBody().toString().contains(response)) {
                         // found a possible injection
-                        raiseAlert(msg, param, payload, response, checkType.resourceIdentifier);
+                        raiseAlert(
+                                msg, param, payload, response, checkType.getResourceIdentifier());
                         return true;
                     }
                 }
@@ -169,24 +182,31 @@ public class XSLTInjection extends AbstractAppParamPlugin {
         return msg;
     }
 
-    private String getXslForPortScan() {
+    private static String getXslForPortScan(HttpMessage msg) {
         // only tests one port for now
         try {
             return String.format(
                     "<xsl:value-of select=\"document('%s')\"/>",
-                    "http://" + getBaseMsg().getRequestHeader().getURI().getHost() + ":22");
+                    "http://" + msg.getRequestHeader().getURI().getHost() + ":22");
         } catch (URIException e) {
-            return new String();
+            LOG.warn(
+                    "An error occurred while getting Host for "
+                            + msg.getRequestHeader().getURI()
+                            + " Caught "
+                            + e.getClass().getName()
+                            + " "
+                            + e.getMessage());
+            return "";
         }
     }
 
     private boolean requestsLimitReached() {
         return strengthToRequestCountMap.containsKey(getAttackStrength())
-                ? false
-                : requestsSent >= strengthToRequestCountMap.get(getAttackStrength());
+                ? requestsSent >= strengthToRequestCountMap.get(getAttackStrength())
+                : false;
     }
 
-    private String getOtherInfo(String resourceIdentifier, String param) {
+    private static String getOtherInfo(String resourceIdentifier, String param) {
         return Constant.messages.getString(
                 MESSAGE_PREFIX + resourceIdentifier + ".otherinfo", param);
     }
