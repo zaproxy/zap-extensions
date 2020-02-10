1a12b17e990eb49b167bd7430694bd531f302f98
Simon Bennetts <psiinon@gmail.com>
2020-02-10 15:51:50 +0000

    Add support for selenium scripts
    Signed-off-by: Simon Bennetts <psiinon@gmail.com>

---
+++ addOns/selenium/src/main/java/org/zaproxy/zap/extension/selenium/SeleniumScriptUtils.java
@@ -0,0 +1,60 @@
+/*
+ * Zed Attack Proxy (ZAP) and its related class files.
+ *
+ * ZAP is an HTTP/HTTPS proxy for assessing web application security.
+ *
+ * Copyright 2020 The ZAP Development Team
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.zaproxy.zap.extension.selenium;
+
+import org.openqa.selenium.WebDriver;
+
+public class SeleniumScriptUtils {
+    private WebDriver wd;
+    private int requester;
+    private String browserId;
+    private String proxyAddress;
+    private int proxyPort;
+
+    public SeleniumScriptUtils(
+            WebDriver wd, int requester, String browserId, String proxyAddress, int proxyPort) {
+        super();
+        this.wd = wd;
+        this.requester = requester;
+        this.browserId = browserId;
+        this.proxyAddress = proxyAddress;
+        this.proxyPort = proxyPort;
+    }
+
+    public WebDriver getWebDriver() {
+        return wd;
+    }
+
+    public int getRequester() {
+        return requester;
+    }
+
+    public String getBrowserId() {
+        return browserId;
+    }
+
+    public String getProxyAddress() {
+        return proxyAddress;
+    }
+
+    public int getProxyPort() {
+        return proxyPort;
+    }
+}
