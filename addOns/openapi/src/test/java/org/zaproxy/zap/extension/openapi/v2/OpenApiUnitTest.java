9b02c3cee4d9d94ce656cb00973db2b21182ceb6
Jannik Hollenbach <13718901+J12934@users.noreply.github.com>
2020-04-20 19:29:36 +0200

    Add expected response for OpenAPI v2 Test

--- addOns/openapi/src/test/java/org/zaproxy/zap/extension/openapi/v2/OpenApiUnitTest.java
+++ addOns/openapi/src/test/java/org/zaproxy/zap/extension/openapi/v2/OpenApiUnitTest.java
@@ -319,7 +319,7 @@ public class OpenApiUnitTest extends AbstractServerTest {
         // Then
         assertThat(converter.getErrorMessages(), is(empty()));
         assertEquals(
-                "{\"id\":10,\"category\":{\"id\":10,\"name\":\"John Doe\"},\"name\":\"John Doe\",\"photoUrls\":[\"John Doe\"],\"tags\":[{\"id\":10,\"name\":\"John Doe\",\"x\":\"John Doe\"}],\"status\":\"available\"}",
+                "{\"id\":10,\"category\":{\"id\":10,\"name\":\"John Doe\"},\"name\":\"doggie\",\"photoUrls\":[\"John Doe\"],\"tags\":[{\"id\":10,\"name\":\"John Doe\",\"x\":\"John Doe\"}],\"status\":\"available\"}",
                 accessedUrls.get("POST http://" + host + "/PetStore/pet"));
     }
 
