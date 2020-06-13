675086144990a6af03ec76a5f0fbdd1136b6d4be
kberg <kc@stackhawk.com>
2020-06-12 19:38:49 -0600

    kotlin addon in java
    Signed-off-by: kberg <kc@stackhawk.com>

--- addOns/kotlin/src/main/zapHomeFiles/scripts/templates/standalone/Standalone default template.kts
+++ addOns/kotlin/src/main/zapHomeFiles/scripts/templates/standalone/Standalone default template.kts
@@ -1,2 +1,3 @@
-
 println("KaaKaawwtlin!!")
+
+
---
+++ addOns/kotlin/src/test/java/org/zaproxy/zap/extension/kotlin/VerifyScriptTemplates.java
@@ -0,0 +1,57 @@
+/*
+ * Zed Attack Proxy (ZAP) and its related class files.
+ *
+ * ZAP is an HTTP/HTTPS proxy for assessing web application security.
+ *
+ * Copyright 2020 The ZAP Development Team
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.zaproxy.zap.extension.kotlin;
+
+import java.io.Reader;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import javax.script.Compilable;
+import javax.script.CompiledScript;
+import org.junit.jupiter.api.BeforeAll;
+import org.zaproxy.zap.testutils.AbstractVerifyScriptTemplates;
+
+/** Verifies that the Jython script templates are parsed without errors. */
+public class VerifyScriptTemplates extends AbstractVerifyScriptTemplates {
+
+    private static Compilable se;
+
+    @BeforeAll
+    public static void setUp() {
+        se =
+                (Compilable)
+                        new KotlinScriptEngineFactory(
+                                        Thread.currentThread().getContextClassLoader(), null)
+                                .getScriptEngine();
+    }
+
+    @Override
+    protected String getScriptExtension() {
+        return ".kts";
+    }
+
+    @Override
+    protected void parseTemplate(Path template) throws Exception {
+        try (Reader reader = Files.newBufferedReader(template, StandardCharsets.UTF_8)) {
+            CompiledScript cs = se.compile(reader);
+            cs.eval();
+        }
+    }
+}
