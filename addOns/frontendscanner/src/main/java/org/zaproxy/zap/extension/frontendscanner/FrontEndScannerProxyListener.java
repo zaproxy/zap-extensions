b70a8045952e13999ba644d08d984897894d4c8b
Pamplemousse <xav.maso@gmail.com>
2020-07-05 15:10:40 -0700

    frontendscanner: Add some unit tests
    To validate the behavior of `onHttpResponseReceive`.
    
    Signed-off-by: Pamplemousse <xav.maso@gmail.com>

---
+++ addOns/frontendscanner/src/main/java/org/zaproxy/zap/extension/frontendscanner/FrontEndScannerProxyListener.java
@@ -0,0 +1,101 @@
+/*
+ * Zed Attack Proxy (ZAP) and its related class files.
+ *
+ * ZAP is an HTTP/HTTPS proxy for assessing web application security.
+ *
+ * Copyright 2020 The ZAP Development Team
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.zaproxy.zap.extension.frontendscanner;
+
+import java.util.List;
+import net.htmlparser.jericho.Element;
+import net.htmlparser.jericho.OutputDocument;
+import net.htmlparser.jericho.Source;
+import org.apache.log4j.Logger;
+import org.parosproxy.paros.core.proxy.ProxyListener;
+import org.parosproxy.paros.extension.history.ProxyListenerLog;
+import org.parosproxy.paros.network.HttpMessage;
+import org.zaproxy.zap.extension.api.API;
+
+/** The {@link ProxyListener} the {@link ExtensionFrontEndScanner} relies on. */
+public class FrontEndScannerProxyListener implements ProxyListener {
+    private static final Logger LOGGER = Logger.getLogger(FrontEndScannerProxyListener.class);
+
+    private final FrontEndScannerAPI api;
+    private final FrontEndScannerOptions options;
+
+    public FrontEndScannerProxyListener(FrontEndScannerAPI api, FrontEndScannerOptions options) {
+        this.api = api;
+        this.options = options;
+    }
+
+    @Override
+    public boolean onHttpRequestSend(HttpMessage msg) {
+        return true;
+    }
+
+    @Override
+    public boolean onHttpResponseReceive(HttpMessage msg) {
+        if (options.isEnabled() && msg.getResponseHeader().isHtml()) {
+            try {
+                String html = msg.getResponseBody().toString();
+
+                Source document = new Source(html);
+                List<Element> heads = document.getAllElements("head");
+                Element head = heads.isEmpty() ? null : heads.get(0);
+
+                if (head != null && msg.getHistoryRef() != null) {
+                    String host = msg.getRequestHeader().getHeader("host");
+                    String frontEndApiUrl =
+                            API.getInstance().getCallBackUrl(this.api, "https://" + host);
+
+                    int historyReferenceId = msg.getHistoryRef().getHistoryId();
+
+                    StringBuilder injectedContentBuilder =
+                            new StringBuilder(200)
+                                    .append("<script src='")
+                                    .append(frontEndApiUrl)
+                                    .append("?action=getFile")
+                                    .append("&filename=front-end-scanner.js")
+                                    .append("&historyReferenceId=")
+                                    .append(historyReferenceId)
+                                    .append("'></script>");
+
+                    String injectedContent = injectedContentBuilder.toString();
+
+                    OutputDocument newResponseBody = new OutputDocument(document);
+                    int insertPosition = head.getChildElements().get(0).getBegin();
+                    newResponseBody.insert(insertPosition, injectedContent);
+
+                    msg.getResponseBody().setBody(newResponseBody.toString());
+
+                    int newLength = msg.getResponseBody().length();
+                    msg.getResponseHeader().setContentLength(newLength);
+                } else {
+                    LOGGER.debug("<head></head> is missing in the response");
+                }
+            } catch (Exception e) {
+                LOGGER.error(e.getMessage(), e);
+            }
+        }
+        return true;
+    }
+
+    @Override
+    public int getArrangeableListenerOrder() {
+        // Need to run after the HistoryReference has been saved to the database
+        return ProxyListenerLog.PROXY_LISTENER_ORDER + 42;
+    }
+}
---
+++ addOns/frontendscanner/src/test/java/org/zaproxy/zap/extension/frontendscanner/FrontEndScannerProxyListenerUnitTest.java
@@ -0,0 +1,79 @@
+/*
+ * Zed Attack Proxy (ZAP) and its related class files.
+ *
+ * ZAP is an HTTP/HTTPS proxy for assessing web application security.
+ *
+ * Copyright 2020 The ZAP Development Team
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.zaproxy.zap.extension.frontendscanner;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import org.apache.commons.httpclient.URI;
+import org.apache.commons.httpclient.URIException;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.parosproxy.paros.model.HistoryReference;
+import org.parosproxy.paros.network.HttpMalformedHeaderException;
+import org.parosproxy.paros.network.HttpMessage;
+import org.parosproxy.paros.network.HttpResponseHeader;
+import org.zaproxy.zap.testutils.TestUtils;
+
+/** Unit test for {@link FrontEndScannerProxyListener}. */
+public class FrontEndScannerProxyListenerUnitTest extends TestUtils {
+
+    private static final String HOSTNAME = "example.com";
+
+    private FrontEndScannerProxyListener frontEndScannerProxyListener;
+    private HttpMessage msg;
+
+    @BeforeEach
+    public void setUp() throws URIException, HttpMalformedHeaderException {
+        FrontEndScannerAPI api = mock(FrontEndScannerAPI.class);
+        FrontEndScannerOptions options = mock(FrontEndScannerOptions.class);
+        when(options.isEnabled()).thenReturn(true);
+
+        HistoryReference ref = mock(HistoryReference.class);
+        when(ref.getHistoryId()).thenReturn(42);
+
+        frontEndScannerProxyListener = new FrontEndScannerProxyListener(api, options);
+
+        msg = new HttpMessage(new URI("https", HOSTNAME, "/", ""));
+        msg.getResponseHeader().setHeader(HttpResponseHeader.CONTENT_TYPE, "text/html");
+        msg.setHistoryRef(ref);
+    }
+
+    @Test
+    public void testInjectTheFrontEndTrackerBeforeOtherScriptsInHeadTag() {
+        // Given
+        String htmlBody =
+                "<!doctype html><html lang='en'><head><script></script></head><body></body></html>";
+        msg.setResponseBody(htmlBody);
+
+        // When
+        frontEndScannerProxyListener.onHttpResponseReceive(msg);
+
+        // Then
+        String expectedHtmlFormat =
+                "<!doctype html><html lang='en'><head><script src='https:\\/\\/"
+                        + HOSTNAME
+                        + "\\/zapCallBackUrl\\/-?[0-9]+\\?action=getFile&filename=front-end-scanner.js&historyReferenceId=42'><\\/script><script><\\/script><\\/head><body><\\/body></html>";
+        String result = msg.getResponseBody().toString();
+
+        assertTrue(result.matches(expectedHtmlFormat));
+    }
+}
