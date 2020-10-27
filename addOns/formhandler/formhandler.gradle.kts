53d2cc339af7fc2512bb72f6a59d4918228a86a4
Simon Bennetts <psiinon@gmail.com>
2020-10-27 12:28:30 +0000

    Form handler: promote to beta
    Signed-off-by: Simon Bennetts <psiinon@gmail.com>

--- addOns/formhandler/formhandler.gradle.kts
+++ addOns/formhandler/formhandler.gradle.kts
@@ -1,8 +1,11 @@
+import org.zaproxy.gradle.addon.AddOnStatus
+
 version = "3"
 description = "This Form Handler Add-on allows a user to define field names and values to be used in a form's fields. Fields can be added, modified, enabled, and deleted for use in form fields."
 
 zapAddOn {
     addOnName.set("Form Handler")
+    addOnStatus.set(AddOnStatus.BETA)
     zapVersion.set("2.9.0")
 
     manifest {
