753a46dc8540cc11fc4c84695ed8da5e598a356e
Simon Bennetts <psiinon@gmail.com>
2020-11-04 10:27:32 +0000

    Webdrivers - prepare for the next release
    Signed-off-by: Simon Bennetts <psiinon@gmail.com>

--- addOns/webdrivers/webdrivermacos/webdrivermacos.gradle.kts
+++ addOns/webdrivers/webdrivermacos/webdrivermacos.gradle.kts
@@ -1,7 +1,7 @@
 import org.zaproxy.gradle.addon.AddOnStatus
 import org.zaproxy.gradle.tasks.DownloadWebDriver
 
-version = "21"
+version = "22"
 description = "MacOS WebDrivers for Firefox and Chrome."
 
 extra["targetOs"] = DownloadWebDriver.OS.MAC
--- addOns/webdrivers/webdriverwindows/webdriverwindows.gradle.kts
+++ addOns/webdrivers/webdriverwindows/webdriverwindows.gradle.kts
@@ -1,7 +1,7 @@
 import org.zaproxy.gradle.addon.AddOnStatus
 import org.zaproxy.gradle.tasks.DownloadWebDriver
 
-version = "22"
+version = "23"
 description = "Windows WebDrivers for Firefox and Chrome."
 
 extra["targetOs"] = DownloadWebDriver.OS.WIN
