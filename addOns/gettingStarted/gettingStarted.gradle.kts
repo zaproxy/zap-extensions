19b31d24ad8b76c6267195d639ab65aa944e10c8
kingthorin <kingthorin@users.noreply.github.com>
2021-02-26 19:46:45 -0500

    gettingStarted: Use Log4j 2.x
    - Update build file, CHANGELOG, and change code to use updated logging
    infrastructure.
    
    Part of: zaproxy/zaproxy#6376
    
    Signed-off-by: kingthorin <kingthorin@users.noreply.github.com>

--- addOns/gettingStarted/gettingStarted.gradle.kts
+++ addOns/gettingStarted/gettingStarted.gradle.kts
@@ -6,11 +6,10 @@ description = "A short Getting Started with ZAP Guide"
 zapAddOn {
     addOnName.set("Getting Started with ZAP Guide")
     addOnStatus.set(AddOnStatus.RELEASE)
-    zapVersion.set("2.9.0")
+    zapVersion.set("2.10.0")
 
     manifest {
         author.set("ZAP Dev Team")
         url.set("https://www.zaproxy.org/docs/desktop/addons/getting-started-guide/")
-        notBeforeVersion.set("2.10.0")
     }
 }
