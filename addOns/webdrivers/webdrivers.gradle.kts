1f50f42e0d12d5f651277f32f4133888ebc07d4e
thc202 <thc202@gmail.com>
2021-03-25 16:22:04 +0000

    webdrivers: update ChromeDriver, prepare releases
    Update ChromeDriver to 89.0.4389.23.
    Add release dates and links to tags.
    
    Signed-off-by: thc202 <thc202@gmail.com>

--- addOns/webdrivers/webdrivers.gradle.kts
+++ addOns/webdrivers/webdrivers.gradle.kts
@@ -5,7 +5,7 @@ import org.zaproxy.gradle.tasks.DownloadWebDriver
 description = "Common configuration of the WebDriver add-ons."
 
 val geckodriverVersion = "0.29.0"
-val chromeDriverVersion = "88.0.4324.96"
+val chromeDriverVersion = "89.0.4389.23"
 
 fun configureDownloadTask(outputDir: File, targetOs: DownloadWebDriver.OS, task: DownloadWebDriver) {
     val geckodriver = task.browser.get() == DownloadWebDriver.Browser.FIREFOX
