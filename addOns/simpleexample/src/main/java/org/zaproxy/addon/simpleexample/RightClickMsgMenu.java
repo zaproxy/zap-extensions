fe707c5ad5e59af2db2a0c215fd63585b519a370
ricekot <ricekot@gmail.com>
2021-04-01 01:00:14 +0530

    simpleexample: Update package name
    - Change package name from "org.zaproxy.zap.extension.simpleexample" to "org.zaproxy.addon.simpleexample"
    
    Signed-off-by: ricekot <ricekot@gmail.com>

--- addOns/simpleexample/src/main/java/org/zaproxy/addon/simpleexample/RightClickMsgMenu.java
+++ addOns/simpleexample/src/main/java/org/zaproxy/addon/simpleexample/RightClickMsgMenu.java
@@ -17,7 +17,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.zaproxy.zap.extension.simpleexample;
+package org.zaproxy.addon.simpleexample;
 
 import org.parosproxy.paros.Constant;
 import org.parosproxy.paros.network.HttpMessage;
