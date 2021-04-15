b70f2cee3563406815a97fd5f36eb350bcf883ec
thc202 <thc202@gmail.com>
2021-04-15 17:24:09 +0100

    webdrivers: prepare next dev iteration
    Update versions and changelogs.
    
    Signed-off-by: thc202 <thc202@gmail.com>

--- addOns/webdrivers/webdriverwindows/webdriverwindows.gradle.kts
+++ addOns/webdrivers/webdriverwindows/webdriverwindows.gradle.kts
@@ -1,7 +1,7 @@
 import org.zaproxy.gradle.addon.AddOnStatus
 import org.zaproxy.gradle.tasks.DownloadWebDriver
 
-version = "28"
+version = "29"
 description = "Windows WebDrivers for Firefox and Chrome."
 
 extra["targetOs"] = DownloadWebDriver.OS.WIN
