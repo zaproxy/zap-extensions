9969f9fe51c5b09c93e6e2c2f81ddb493abe0096
ricekot <ricekot@gmail.com>
2021-06-18 17:42:29 +0530

    oast: Initial commit
    Signed-off-by: ricekot <ricekot@gmail.com>

---
+++ addOns/oast/oast.gradle.kts
@@ -0,0 +1,16 @@
+description = "OAST Support: Exploit Out-Of-Band Vulnerabilities"
+
+zapAddOn {
+    addOnName.set("OAST Support")
+    zapVersion.set("2.10.0")
+
+    manifest {
+        author.set("ZAP Dev Team")
+        url.set("https://www.zaproxy.org/docs/desktop/addons/oast-support/")
+    }
+
+    apiClientGen {
+        api.set("org.zaproxy.addon.oast.OastApi")
+        messages.set(file("src/main/resources/org/zaproxy/addon/oast/resources/Messages.properties"))
+    }
+}
