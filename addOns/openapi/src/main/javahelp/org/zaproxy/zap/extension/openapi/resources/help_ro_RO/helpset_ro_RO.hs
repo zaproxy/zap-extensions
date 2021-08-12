1178bb6c94c5eae5da0cb61623dfeeca5ee17099
zapbot <12745184+zapbot@users.noreply.github.com>
2021-08-12 19:59:09 +0000

    Update localized resources
    Update resources from Crowdin.
    
    Signed-off-by: zapbot <12745184+zapbot@users.noreply.github.com>

--- addOns/openapi/src/main/javahelp/org/zaproxy/zap/extension/openapi/resources/help_ro_RO/helpset_ro_RO.hs
+++ addOns/openapi/src/main/javahelp/org/zaproxy/zap/extension/openapi/resources/help_ro_RO/helpset_ro_RO.hs
@@ -1,10 +1,9 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<!DOCTYPE helpset PUBLIC "-//Sun Microsystems Inc.//DTD JavaHelp HelpSet Version 2.0//EN" "http://java.sun.com/products/javahelp/helpset_2_0.dtd">
+<?xml version="1.0" encoding="UTF-8"?><!DOCTYPE helpset PUBLIC "-//Sun Microsystems Inc.//DTD JavaHelp HelpSet Version 2.0//EN" "http://java.sun.com/products/javahelp/helpset_2_0.dtd">
 <helpset version="2.0" xml:lang="ro-RO">
-  <title>Support for the Open API Specification | ZAP Extension</title>
+  <title>OpenAPI Support Add-on</title>
 
   <maps>
-     <homeID>top</homeID>
+     <homeID>openapi</homeID>
      <mapref location="map.jhm"/>
   </maps>
 
