a507823121cf17b4b804dddbdeb8d03687f60e31
Josh Soref <jsoref@users.noreply.github.com>
2021-08-24 17:34:50 -0400

    spelling: addOns/domxss
    * pollute
    
    Signed-off-by: Josh Soref <jsoref@users.noreply.github.com>

--- addOns/domxss/src/main/java/org/zaproxy/zap/extension/domxss/DomXssScanRule.java
+++ addOns/domxss/src/main/java/org/zaproxy/zap/extension/domxss/DomXssScanRule.java
@@ -202,7 +202,7 @@ public class DomXssScanRule extends AbstractAppParamPlugin {
     }
 
     /*
-     * We use a separate port so that we dont polute the sites tree
+     * We use a separate port so that we dont pollute the sites tree
      * and show the requests in the Active Scan tab
      */
     private ProxyServer getProxy() {
