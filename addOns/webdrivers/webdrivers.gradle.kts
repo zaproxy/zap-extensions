1a830fb74a50c70ec32fd5ee4ca64b1ae7ad61b6
thc202 <thc202@gmail.com>
2021-09-07 10:46:37 +0100

    webdrivers: update ChromeDriver
    Update ChromeDriver to 93.0.4577.15.
    
    Signed-off-by: thc202 <thc202@gmail.com>

--- addOns/webdrivers/webdrivers.gradle.kts
+++ addOns/webdrivers/webdrivers.gradle.kts
@@ -6,7 +6,7 @@ import org.zaproxy.gradle.tasks.DownloadWebDriver
 description = "Common configuration of the WebDriver add-ons."
 
 val geckodriverVersion = "0.29.1"
-val chromeDriverVersion = "91.0.4472.101"
+val chromeDriverVersion = "93.0.4577.15"
 
 fun configureDownloadTask(outputDir: File, targetOs: DownloadWebDriver.OS, task: DownloadWebDriver) {
     val geckodriver = task.browser.get() == DownloadWebDriver.Browser.FIREFOX
