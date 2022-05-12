3ee5907c2e1b70f55cdbb689606ee5e376cb5496
kingthorin <kingthorin@users.noreply.github.com>
2022-05-12 10:30:47 -0400

    feat: Add Export menu and move Export URL items from core
    - ExtensionExim.java > Instantiate the menu items when they are
    deprecated in core.
    - Messages.properties > Add support key/value pairs.
    - exim.html > Updated help content.
    - PopUpMenuExport*Urls.java & PopupMenuCopyUrls.java > Copied from core
    with minor adjustments for new location and clean code.
    - PopupMenuSaveRawMessage > Now includes an "All" option to save the
    entire message.
    - PopupMenuExportMessages > Replacement component for the core Export
    Messages and Export Responses functionality.
    
    Signed-off-by: kingthorin <kingthorin@users.noreply.github.com>

--- addOns/exim/CHANGELOG.md
+++ addOns/exim/CHANGELOG.md
@@ -8,6 +8,12 @@ The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/).
 - Tweaked import functionality to mark import progress components completed when an exception occurs during import (thus allowing them to be cleared properly).
 - HAR imports will now use an indeterminate progress bar if the count of entries cannot be determined.
 
+### Added
+- Copy URLs, Export Context URLs, Export Selected URLs, Export Messages, and Export Responses functionality similar to what was previously offered via core functionality.
+
+### Changed
+- Save RAW functionality now includes an All option which saves the entire HTTP message.
+
 ## [0.1.0] - 2022-03-07
 ### Changed
 - Reduce logging and display a warning dialog when unable to read files being imported (Issue 7081).
--- addOns/exim/src/main/java/org/zaproxy/addon/exim/ExtensionExim.java
+++ addOns/exim/src/main/java/org/zaproxy/addon/exim/ExtensionExim.java
@@ -22,11 +22,14 @@ package org.zaproxy.addon.exim;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import javax.swing.JMenu;
 import org.parosproxy.paros.Constant;
 import org.parosproxy.paros.control.Control;
 import org.parosproxy.paros.extension.Extension;
 import org.parosproxy.paros.extension.ExtensionAdaptor;
 import org.parosproxy.paros.extension.ExtensionHook;
+import org.parosproxy.paros.extension.history.ExtensionHistory;
+import org.parosproxy.paros.view.MainMenuBar;
 import org.parosproxy.paros.view.View;
 import org.zaproxy.addon.commonlib.ExtensionCommonlib;
 import org.zaproxy.addon.commonlib.ui.ProgressPanel;
@@ -42,6 +45,15 @@ public class ExtensionExim extends ExtensionAdaptor {
     private static final String NAME = "ExtensionExim";
     private static final List<Class<? extends Extension>> DEPENDENCIES =
             Collections.unmodifiableList(Arrays.asList(ExtensionCommonlib.class));
+
+    private JMenu menuExport;
+
+    private PopupMenuExportMessages popupMenuExportResponses;
+    private PopupMenuExportMessages popupMenuExportMessages;
+    private PopupMenuExportContextUrls popupMenuExportContextUrls;
+    private PopupMenuExportSelectedUrls popupMenuExportSelectedrls;
+    private PopupMenuExportUrls popupMenuExportUrls;
+    private PopupMenuCopyUrls popupMenuCopyUrls;
 
     public ExtensionExim() {
         super(NAME);
@@ -61,11 +73,38 @@ public class ExtensionExim extends ExtensionAdaptor {
             extensionHook.getHookMenu().addPopupMenuItem(new PopupMenuSaveXmlMessage());
             extensionHook.getHookMenu().addPopupMenuItem(new PopupMenuItemSaveHarMessage());
 
+            // If this is deprecated then the others are as well, due to timing/co-ordination
+            if (isDeprecated(org.zaproxy.zap.extension.stdmenus.PopupMenuCopyUrls.class)) {
+
+                if (getExtensionHistory() != null) {
+                    getMenuExport().add(getPopupMenuExportMessages());
+                    getMenuExport().add(getPopupMenuExportResponses());
+                }
+
+                extensionHook.getHookMenu().addPopupMenuItem(getPopupMenuExportContextUrls());
+                getMenuExport().add(getPopupMenuExportContextUrls());
+
+                extensionHook.getHookMenu().addPopupMenuItem(getPopupMenuExportSelectedUrls());
+                getMenuExport().add(getPopupMenuExportSelectedUrls());
+
+                extensionHook.getHookMenu().addPopupMenuItem(getPopupMenuExportUrls());
+                getMenuExport().add(getPopupMenuExportUrls());
+
+                extensionHook.getHookMenu().addPopupMenuItem(getPopupMenuCopyUrls());
+            }
+
+            MainMenuBar menuBar = getView().getMainFrame().getMainMenuBar();
+            menuBar.add(getMenuExport(), menuBar.getMenuCount() - 2); // Before Online and Help
+
             extensionHook.getHookMenu().addImportMenuItem(new MenuImportHar());
             extensionHook.getHookMenu().addImportMenuItem(new MenuItemImportUrls());
             extensionHook.getHookMenu().addImportMenuItem(new MenuItemImportLogs());
         }
         extensionHook.addApiImplementor(new ImportExportApi());
+    }
+
+    private static boolean isDeprecated(Class<?> classToCheck) {
+        return classToCheck.getAnnotation(Deprecated.class) != null;
     }
 
     @Override
@@ -83,6 +122,14 @@ public class ExtensionExim extends ExtensionAdaptor {
         return true;
     }
 
+    @Override
+    public void unload() {
+        if (hasView()) {
+            MainMenuBar menuBar = getView().getMainFrame().getMainMenuBar();
+            menuBar.remove(getMenuExport());
+        }
+    }
+
     public static void updateOutput(String messageKey, String filePath) {
         if (View.isInitialised()) {
             StringBuilder sb = new StringBuilder(128);
@@ -96,5 +143,67 @@ public class ExtensionExim extends ExtensionAdaptor {
                 .getExtensionLoader()
                 .getExtension(ExtensionCommonlib.class)
                 .getProgressPanel();
+    }
+
+    private JMenu getMenuExport() {
+        if (menuExport == null) {
+            menuExport = new JMenu();
+            menuExport.setText(Constant.messages.getString("exim.menu.export"));
+            menuExport.setMnemonic(Constant.messages.getChar("exim.menu.export.mnemonic"));
+        }
+        return menuExport;
+    }
+
+    private PopupMenuExportMessages getPopupMenuExportMessages() {
+        if (popupMenuExportMessages == null) {
+            popupMenuExportMessages = new PopupMenuExportMessages(getExtensionHistory(), false);
+        }
+        return popupMenuExportMessages;
+    }
+
+    private PopupMenuExportMessages getPopupMenuExportResponses() {
+        if (popupMenuExportResponses == null) {
+            popupMenuExportResponses = new PopupMenuExportMessages(getExtensionHistory(), true);
+        }
+        return popupMenuExportResponses;
+    }
+
+    private PopupMenuExportContextUrls getPopupMenuExportContextUrls() {
+        if (popupMenuExportContextUrls == null) {
+            popupMenuExportContextUrls =
+                    new PopupMenuExportContextUrls(
+                            Constant.messages.getString("exim.menu.export.context.urls"), this);
+        }
+        return popupMenuExportContextUrls;
+    }
+
+    private PopupMenuExportSelectedUrls getPopupMenuExportSelectedUrls() {
+        if (popupMenuExportSelectedrls == null) {
+            popupMenuExportSelectedrls =
+                    new PopupMenuExportSelectedUrls(
+                            Constant.messages.getString("exim.menu.export.popup.selected"), this);
+        }
+        return popupMenuExportSelectedrls;
+    }
+
+    private PopupMenuExportUrls getPopupMenuExportUrls() {
+        if (popupMenuExportUrls == null) {
+            popupMenuExportUrls =
+                    new PopupMenuExportSelectedUrls(
+                            Constant.messages.getString("exim.menu.export.popup"), this);
+        }
+        return popupMenuExportUrls;
+    }
+
+    private PopupMenuCopyUrls getPopupMenuCopyUrls() {
+        if (popupMenuCopyUrls == null) {
+            popupMenuCopyUrls =
+                    new PopupMenuCopyUrls(Constant.messages.getString("exim.menu.copyurls.popup"));
+        }
+        return popupMenuCopyUrls;
+    }
+
+    private static ExtensionHistory getExtensionHistory() {
+        return Control.getSingleton().getExtensionLoader().getExtension((ExtensionHistory.class));
     }
 }
