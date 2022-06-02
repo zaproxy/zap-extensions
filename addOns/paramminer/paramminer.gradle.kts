086bdb2499b6c6cbf59c1972e1d8554009ed46c1
ArkaprabhaChakraborty <chakrabortyarkaprabha998@gmail.com>
2022-06-02 16:31:22 +0530

    Add initial param miner extension
    Signed-off-by: ArkaprabhaChakraborty <chakrabortyarkaprabha998@gmail.com>

---
+++ addOns/paramminer/paramminer.gradle.kts
@@ -0,0 +1,18 @@
+description = "Identify hidden, unlinked parameters. Useful for finding web cache poisoning vulnerabilities."
+
+zapAddOn {
+    addOnName.set("Parameter Miner")
+    zapVersion.set("2.11.1")
+
+    manifest {
+        author.set("ZAP Dev Team and Arkaprabha Chakraborty")
+    }
+}
+
+crowdin {
+    configuration {
+        val resourcesPath = "org/zaproxy/addon/${zapAddOn.addOnId.get()}/resources/"
+        tokens.put("%messagesPath%", resourcesPath)
+        tokens.put("%helpPath%", resourcesPath)
+    }
+}
