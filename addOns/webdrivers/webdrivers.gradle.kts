f06499f12711551d93447b009cc24dad6bbee0b3
zapbot <12745184+zapbot@users.noreply.github.com>
2022-07-20 23:18:01 +0000

    Webdriver Update
    ChromeDriver update to 103.0.5060.134
    
    Signed-off-by: zapbot <12745184+zapbot@users.noreply.github.com>

--- addOns/webdrivers/webdrivers.gradle.kts
+++ addOns/webdrivers/webdrivers.gradle.kts
@@ -6,7 +6,7 @@ import org.zaproxy.gradle.tasks.DownloadWebDriver
 description = "Common configuration of the WebDriver add-ons."
 
 val geckodriverVersion = "0.31.0"
-val chromeDriverVersion = "103.0.5060.53"
+val chromeDriverVersion = "103.0.5060.134"
 
 fun configureDownloadTask(outputDir: File, targetOs: DownloadWebDriver.OS, task: DownloadWebDriver) {
     val geckodriver = task.browser.get() == DownloadWebDriver.Browser.FIREFOX
