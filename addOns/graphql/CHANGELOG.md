9f790a3ba8f0fefc120303056ec0e96058917a39
vinicio ferreira <vinicio.ferreira@cvortex.io>
2022-08-04 16:16:19 -0300

    graphql: update dependency
    Update GraphQL Java to latest version (19.0), with important bugfixes.

--- addOns/graphql/CHANGELOG.md
+++ addOns/graphql/CHANGELOG.md
@@ -5,6 +5,7 @@ The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/) 
 
 ## Unreleased
 ### Changed
+- Update graphql-java dependency, with important bugfixes.
 - Maintenance changes.
 - Update dependency, which reduces add-on file size (Issue 7322).
 
--- addOns/graphql/graphql.gradle.kts
+++ addOns/graphql/graphql.gradle.kts
@@ -41,7 +41,7 @@ crowdin {
 dependencies {
     compileOnly(parent!!.childProjects.get("automation")!!)
     implementation("com.google.code.gson:gson:2.8.8")
-    implementation("com.graphql-java:graphql-java:18.2")
+    implementation("com.graphql-java:graphql-java:19.0")
 
     testImplementation(parent!!.childProjects.get("automation")!!)
     testImplementation(project(":testutils"))
