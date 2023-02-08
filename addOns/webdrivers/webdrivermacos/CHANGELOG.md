53f5283b5befba7cd0baf5c9a9be1a3af95bd49d
zapbot <12745184+zapbot@users.noreply.github.com>
2023-02-08 14:22:00 +0000

    Webdriver Update
    geckodriver update to 0.32.2
    
    Signed-off-by: zapbot <12745184+zapbot@users.noreply.github.com>

--- addOns/webdrivers/webdrivermacos/CHANGELOG.md
+++ addOns/webdrivers/webdrivermacos/CHANGELOG.md
@@ -5,6 +5,7 @@ The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/).
 
 ## Unreleased
 ### Changed
+- Update geckodriver to 0.32.2.
 - Update ChromeDriver to 110.0.5481.77.
 
 
--- addOns/webdrivers/webdrivers.gradle.kts
+++ addOns/webdrivers/webdrivers.gradle.kts
@@ -5,7 +5,7 @@ import org.zaproxy.gradle.tasks.DownloadWebDriver
 
 description = "Common configuration of the WebDriver add-ons."
 
-val geckodriverVersion = "0.32.1"
+val geckodriverVersion = "0.32.2"
 val chromeDriverVersion = "110.0.5481.77"
 
 fun configureDownloadTask(outputDir: File, targetOs: DownloadWebDriver.OS, task: DownloadWebDriver) {
