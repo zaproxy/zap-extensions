e52251ff5aa2ebc02a8333447fecb319afb6f97b
ArkaprabhaChakraborty <chakrabortyarkaprabha998@gmail.com>
2023-02-11 03:14:22 +0530

    Add note option name and loggers
    Signed-off-by: ArkaprabhaChakraborty <chakrabortyarkaprabha998@gmail.com>

--- addOns/paramdigger/src/main/java/org/zaproxy/addon/paramdigger/gui/ParamDiggerPopUpMenuNote.java
+++ addOns/paramdigger/src/main/java/org/zaproxy/addon/paramdigger/gui/ParamDiggerPopUpMenuNote.java
@@ -19,34 +19,38 @@
  */
 package org.zaproxy.addon.paramdigger.gui;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.parosproxy.paros.Constant;
 import org.parosproxy.paros.extension.history.ExtensionHistory;
 import org.parosproxy.paros.model.HistoryReference;
+import org.zaproxy.zap.utils.DisplayUtils;
 import org.zaproxy.zap.view.messagecontainer.http.HttpMessageContainer;
 import org.zaproxy.zap.view.popup.PopupMenuItemHistoryReferenceContainer;
 
 @SuppressWarnings("serial")
 public class ParamDiggerPopUpMenuNote extends PopupMenuItemHistoryReferenceContainer {
     private final ExtensionHistory ext;
+    private static final Logger LOGGER = LogManager.getLogger(ParamDiggerPopUpMenuNote.class);
 
     public ParamDiggerPopUpMenuNote(ExtensionHistory extensionHistory) {
-        super("paramdigger.popup.note");
+        super(Constant.messages.getString("paramdigger.popup.note"), true);
         this.ext = extensionHistory;
+        super.setSize(DisplayUtils.getScaledDimension(200, 20));
     }
 
     @Override
     public void performAction(HistoryReference href) {
         try {
-            System.out.println("Performing action PARAM DIGGER NOTE");
             ext.showNotesAddDialog(href, href.getHttpMessage().getNote());
         } catch (Exception e) {
-            System.out.println("Exception " + e.getMessage());
+            LOGGER.error(e.getMessage(), e);
         }
     }
 
     @Override
     protected boolean isEnableForInvoker(
             Invoker invoker, HttpMessageContainer httpMessageContainer) {
-        // TODO Auto-generated method stub
         return true;
     }
 }
