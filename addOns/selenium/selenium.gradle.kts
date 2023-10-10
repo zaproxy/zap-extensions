200277c844226cb7a4e0f0f482a44337b35cef7d
thc202 <thc202@gmail.com>
2023-10-10 09:04:37 +0100

    selenium: update Selenium library
    Update Selenium library to version 4.14.0.
    Update the htmlunit-driver to latest version 4.13.0.
    Remove Netty exclusions and JDK HTTP client dependency, the latter is
    now the default.
    Bundle the Selenium libraries for proper inclusion of metadata files
    which contain the declaration of the supported CDP versions.
    
    Signed-off-by: thc202 <thc202@gmail.com>

--- addOns/selenium/selenium.gradle.kts
+++ addOns/selenium/selenium.gradle.kts
@@ -2,6 +2,9 @@ import org.zaproxy.gradle.addon.AddOnStatus
 
 description = "WebDriver provider and includes HtmlUnit browser"
 
+val selenium by configurations.creating
+configurations.api { extendsFrom(selenium) }
+
 zapAddOn {
     addOnName.set("Selenium")
     addOnStatus.set(AddOnStatus.RELEASE)
@@ -17,6 +20,10 @@ zapAddOn {
                 }
             }
         }
+
+        bundledLibs {
+            libs.from(selenium)
+        }
     }
 
     apiClientGen {
@@ -27,14 +34,9 @@ zapAddOn {
 }
 
 dependencies {
-    var seleniumVersion = "4.13.0"
-    api("org.seleniumhq.selenium:selenium-java:$seleniumVersion") {
-        exclude(group = "io.netty")
-    }
-    implementation("org.seleniumhq.selenium:selenium-http-jdk-client:$seleniumVersion")
-    api("org.seleniumhq.selenium:htmlunit-driver:4.12.0") {
-        exclude(group = "io.netty")
-    }
+    var seleniumVersion = "4.14.0"
+    selenium("org.seleniumhq.selenium:selenium-java:$seleniumVersion")
+    selenium("org.seleniumhq.selenium:htmlunit-driver:4.13.0")
     implementation(libs.log4j.slf4j) {
         // Provided by ZAP.
         exclude(group = "org.apache.logging.log4j")
