34043552a4b4e28e996dcf23664fb7283681a7e0
thc202 <thc202@gmail.com>
2023-10-12 19:46:21 +0100

    Remove usage of ZAP snapshot
    Use the main release which is already available.
    
    Signed-off-by: thc202 <thc202@gmail.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,9 +16,6 @@ allprojects {
 
     repositories {
         mavenCentral()
-        maven {
-            url = uri("https://oss.sonatype.org/content/repositories/snapshots/")
-        }
     }
 
     spotless {
