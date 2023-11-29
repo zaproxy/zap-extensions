7b6418c9fb92f4d7ef5a49de981410fe4243c907
karthikuj <karthikuj2001@gmail.com>
2023-11-29 21:34:43 +0530

    Adds link to alert ID
    Signed-off-by: karthikuj <karthikuj2001@gmail.com>

--- addOns/pscanrulesAlpha/src/test/java/org/zaproxy/zap/extension/pscanrulesAlpha/SameOriginMethodExecutionScanRuleTest.java
+++ addOns/pscanrulesAlpha/src/test/java/org/zaproxy/zap/extension/pscanrulesAlpha/SameOriginMethodExecutionScanRuleTest.java
@@ -24,10 +24,12 @@ import static org.hamcrest.Matchers.containsString;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.hasSize;
 
+import java.util.TreeSet;
 import org.apache.commons.httpclient.URI;
 import org.apache.commons.httpclient.URIException;
 import org.junit.jupiter.api.Test;
 import org.parosproxy.paros.core.scanner.Alert;
+import org.parosproxy.paros.network.HtmlParameter;
 import org.parosproxy.paros.network.HttpHeader;
 import org.parosproxy.paros.network.HttpMessage;
 import org.parosproxy.paros.network.HttpRequestHeader;
@@ -98,7 +100,7 @@ class SameOriginMethodExecutionScanRuleTest
         requestUri.setPath("/awesome/jsonpendpoint");
         requestUri.setQuery("callback=swap");
         msg.getRequestHeader()
-                .setHeader(HttpHeader.REFERER, "http://example.com/spa/callback=swap");
+                .setHeader(HttpHeader.REFERER, "http://example.com/spa/?callback=swap");
         msg.getResponseHeader().setStatusCode(HttpStatusCode.OK);
         msg.getResponseHeader().setHeader(HttpHeader.CONTENT_TYPE, HttpHeader.JSON_CONTENT_TYPE);
         msg.setResponseBody("/**/swap({'foobar':'foo'});");
@@ -108,7 +110,8 @@ class SameOriginMethodExecutionScanRuleTest
 
         // Then
         assertThat(alertsRaised, hasSize(1));
-        assertThat(alertsRaised.get(0).getParam(), equalTo("http://example.com/spa/callback=swap"));
+        assertThat(
+                alertsRaised.get(0).getParam(), equalTo("http://example.com/spa/?callback=swap"));
         assertThat(alertsRaised.get(0).getConfidence(), equalTo(Alert.CONFIDENCE_MEDIUM));
         assertThat(alertsRaised.get(0).getEvidence(), equalTo("swap"));
         assertThat(
@@ -123,7 +126,7 @@ class SameOriginMethodExecutionScanRuleTest
         requestUri.setPath("/awesome/jsonpendpoint");
         requestUri.setQuery("callback=swap");
         msg.getRequestHeader()
-                .setHeader(HttpHeader.REFERER, "http://example.com/spa/callback=swap");
+                .setHeader(HttpHeader.REFERER, "http://example.com/spa/?callback=swap");
         msg.getRequestHeader().setHeader("Sec-Fetch-Dest", "script");
         msg.getResponseHeader().setStatusCode(HttpStatusCode.OK);
         msg.getResponseHeader().setHeader(HttpHeader.CONTENT_TYPE, HttpHeader.JSON_CONTENT_TYPE);
@@ -134,10 +137,95 @@ class SameOriginMethodExecutionScanRuleTest
 
         // Then
         assertThat(alertsRaised, hasSize(1));
-        assertThat(alertsRaised.get(0).getParam(), equalTo("http://example.com/spa/callback=swap"));
+        assertThat(
+                alertsRaised.get(0).getParam(), equalTo("http://example.com/spa/?callback=swap"));
         assertThat(alertsRaised.get(0).getConfidence(), equalTo(Alert.CONFIDENCE_HIGH));
         assertThat(alertsRaised.get(0).getEvidence(), equalTo("swap"));
         assertThat(
                 alertsRaised.get(0).getOtherInfo(), containsString("This request originated from"));
+    }
+
+    @Test
+    void shouldRaiseAlertWhenCallbackIsUrlEncodedInsideAnotherParameter() throws Exception {
+        // Given
+        HtmlParameter getParam =
+                new HtmlParameter(HtmlParameter.Type.url, "q", "&callback=callval#");
+        TreeSet<HtmlParameter> allGetParams =
+                new TreeSet<>() {
+                    {
+                        add(getParam);
+                    }
+                };
+        HttpMessage msg = createMessage();
+        msg.setGetParams(allGetParams);
+        msg.setResponseBody(
+                "<html><head></head><body><script src='/api/jsonp?q=&callback=callval#&callback=callbackFunc'></script></body></html>");
+        msg.getResponseHeader().setHeader(HttpHeader.CONTENT_TYPE, DEFAULT_CONTENT_TYPE);
+
+        // When
+        scanHttpResponseReceive(msg);
+
+        // Then
+        assertThat(alertsRaised, hasSize(1));
+        assertThat(alertsRaised.get(0).getParam(), equalTo("callback"));
+        assertThat(alertsRaised.get(0).getConfidence(), equalTo(Alert.CONFIDENCE_MEDIUM));
+        assertThat(alertsRaised.get(0).getEvidence(), equalTo("callback=callval"));
+    }
+
+    @Test
+    void shouldNotRaiseAlertsForWhenParamValueIsEmpty() throws Exception {
+        // Given
+        HttpMessage msg = createMessage();
+        msg.getRequestHeader().getURI().setQuery("toCall=");
+        msg.setResponseBody(
+                "<html><head></head><body><script src='/api/jsonp?callback=swap'></script></body></html>");
+        msg.getResponseHeader().setHeader(HttpHeader.CONTENT_TYPE, DEFAULT_CONTENT_TYPE);
+
+        // When
+        scanHttpResponseReceive(msg);
+
+        // Then
+        assertThat(alertsRaised, hasSize(0));
+    }
+
+    @Test
+    void shouldNotRaiseAlertsForValidJsonResponse() throws Exception {
+        // Given
+        HttpMessage msg = createMessage();
+        URI requestUri = msg.getRequestHeader().getURI();
+        requestUri.setPath("/awesome/validJson");
+        requestUri.setQuery("callback=swap");
+        msg.getRequestHeader()
+                .setHeader(HttpHeader.REFERER, "http://example.com/spa/?callback=swap");
+        msg.getResponseHeader().setStatusCode(HttpStatusCode.OK);
+        msg.getResponseHeader().setHeader(HttpHeader.CONTENT_TYPE, HttpHeader.JSON_CONTENT_TYPE);
+        msg.setResponseBody("{\"validJsonData\": \"swap({'foobar':'foo'});\"}");
+
+        // When
+        scanHttpResponseReceive(msg);
+
+        // Then
+        assertThat(alertsRaised, hasSize(0));
+    }
+
+    @Test
+    void shouldNotRaiseAlertsForSfdHeaderWithValueOtherThanNullOrScript() throws Exception {
+        // Given
+        HttpMessage msg = createMessage();
+        URI requestUri = msg.getRequestHeader().getURI();
+        requestUri.setPath("/awesome/validJson");
+        requestUri.setQuery("callback=swap");
+        msg.getRequestHeader()
+                .setHeader(HttpHeader.REFERER, "http://example.com/spa/?callback=swap");
+        msg.getRequestHeader().setHeader("Sec-Fetch-Dest", "document");
+        msg.getResponseHeader().setStatusCode(HttpStatusCode.OK);
+        msg.getResponseHeader().setHeader(HttpHeader.CONTENT_TYPE, HttpHeader.JSON_CONTENT_TYPE);
+        msg.setResponseBody("/**/swap({'foobar':'foo'});");
+
+        // When
+        scanHttpResponseReceive(msg);
+
+        // Then
+        assertThat(alertsRaised, hasSize(0));
     }
 }
