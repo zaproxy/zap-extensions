2dbb0f917ffaafa8d86bed2b915e6b2b4ae97d78
aryangupta701 <garyan447@gmail.com>
2024-01-14 01:36:20 +0530

    update gradle files to import break addon
    Signed-off-by: aryangupta701 <garyan447@gmail.com>

--- addOns/brk/brk.gradle.kts
+++ addOns/brk/brk.gradle.kts
@@ -1,40 +1,13 @@
-description = "Exposes client (browser) side information in ZAP using Firefox and Chrome extensions."
+description = "Allows you to add breakpoints"
 
 zapAddOn {
-    addOnName.set("Break")
-
+    addOnName.set("brk")
     manifest {
         author.set("ZAP Dev Team")
         url.set("https://www.zaproxy.org/docs/desktop/addons/brk/")
-        extensions {
-        
-        }
-        dependencies {
-            addOns {
-                register("selenium") {
-                    version.set(">=15.14.0")
-                }
-                register("network") {
-                    version.set(">=0.8.0")
-                }
-                register("commonlib")
-            }
-        }
-    }
-}
-
-crowdin {
-    configuration {
-        val resourcesPath = "org/zaproxy/addon/${zapAddOn.addOnId.get()}/resources/"
-        tokens.put("%messagesPath%", resourcesPath)
-        tokens.put("%helpPath%", resourcesPath)
     }
 }
 
 dependencies {
-    zapAddOn("commonlib")
-    zapAddOn("selenium")
-    zapAddOn("network")
-
     testImplementation(project(":testutils"))
 }
