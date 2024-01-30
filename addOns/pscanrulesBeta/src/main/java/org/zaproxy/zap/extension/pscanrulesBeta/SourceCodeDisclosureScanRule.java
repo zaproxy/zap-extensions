2c75a0b0ff4de03fee539d8a152d8be0eaf37129
Aditya Parihar <97739299+PariharAditya@users.noreply.github.com>
2024-01-30 19:24:40 +0530

    pscanrulesBeta: add website links to help pages  (#5249)
    Signed-off-by: Aditya Parihar <97739299+PariharAditya@users.noreply.github.com>

--- addOns/pscanrulesBeta/src/main/java/org/zaproxy/zap/extension/pscanrulesBeta/SourceCodeDisclosureScanRule.java
+++ addOns/pscanrulesBeta/src/main/java/org/zaproxy/zap/extension/pscanrulesBeta/SourceCodeDisclosureScanRule.java
@@ -40,7 +40,8 @@ import org.zaproxy.zap.extension.pscan.PluginPassiveScanner;
  *
  * @author 70pointer@gmail.com
  */
-public class SourceCodeDisclosureScanRule extends PluginPassiveScanner {
+public class SourceCodeDisclosureScanRule extends PluginPassiveScanner
+        implements CommonPassiveScanRuleInfo {
 
     private static final Logger LOGGER = LogManager.getLogger(SourceCodeDisclosureScanRule.class);
 
