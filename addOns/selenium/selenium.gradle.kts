3f87acf06a8f54ea5135ab363b8d69a0ae0784ca
Patrick Double <pat@patdouble.com>
2024-03-07 12:04:43 -0600

    remove redundant commonlib from selenium extension

--- addOns/selenium/selenium.gradle.kts
+++ addOns/selenium/selenium.gradle.kts
@@ -15,9 +15,6 @@ zapAddOn {
 
         dependencies {
             addOns {
-                register("commonlib") {
-                    version.set(">= 1.17.0 & < 2.0.0")
-                }
                 register("network") {
                     version.set(">=0.2.0")
                 }
