3e65f6539aaca5e5f880e2905a8bac8e01b730ec
Moeez905 <x311099@gmail.com>
2024-05-15 18:50:25 +0500

    API Integration
    Signed-off-by: Moeez905 <x311099@gmail.com>

--- addOns/webuipoc/src/main/pocs/web-ui/src/App.js
+++ addOns/webuipoc/src/main/pocs/web-ui/src/App.js
@@ -1,12 +1,36 @@
 
 import './App.css';
+import React, { useState } from 'react';
+import axios from 'axios';
 
-function App() {
-  return (
-   <div>
-    <h1 className='text-3xl text-blue-600'>ZAP Tailwind CSS Check.</h1>
-   </div>
-  );
+const App = () => {
+    const [childNode,SetChildNode] = useState(null);
+    const WebClickZAP = async () => {
+        try {
+            const response = await axios.get('http://localhost:1337/JSON/core/view/childNodes/');
+            SetChildNode(response.data.childNodes);
+            console.log(childNode)
+        } catch (error) {
+            console.error('Error fetching data:', error);
+        }
+    };
+ return (
+        <div className="ZAP-1">
+            <div className="w-full">
+                <div className="flex flex-row justify-between text-center">
+                   <div className=" w-1/3 p-4 font-serif text-center ">Website</div>
+                </div>            
+                <div className="flex flex-row justify-between text-center">
+                    <div className="w-1/3 p-4" onClick={WebClickZAP}>
+                      <p className='font-mono'>Click to fetch</p>
+                      {childNode && childNode.map((node) => (
+                     <p className=''>{node.name}</p>
+                      ))}
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App; 
