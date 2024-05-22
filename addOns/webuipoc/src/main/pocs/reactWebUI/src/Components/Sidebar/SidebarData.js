91192d08aec6fb88b23fc1591147a5ef0131f8e3
Moeez905 <x311099@gmail.com>
2024-05-22 11:55:39 +0500

    Add sidebar component
    Signed-off-by: Moeez905 <x311099@gmail.com>

---
+++ addOns/webuipoc/src/main/pocs/reactWebUI/src/Components/Sidebar/SidebarData.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import HomeIcon from '@mui/icons-material/Home';
+import SettingsIcon from '@mui/icons-material/Settings';
+import HighlightOffIcon from '@mui/icons-material/HighlightOff';
+
+
+export const SidebarData = [ 
+
+    {
+        title: "Home",
+        icon: <HomeIcon />,
+        link: "/home"
+    } ,
+
+        {
+        title: "Settings",
+        icon: <SettingsIcon />,
+        link: "/home"
+    } ,
+
+        {
+        title: "Quit",
+        icon: <HighlightOffIcon />,
+        link: "/home"
+    } 
+
+
+
+]
+
