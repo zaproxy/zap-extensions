7bd51ae9c27e73f9bf8262217be6a279c1d93b5e
Moeez Ahmed <x311099@gmail.com>
2024-06-14 19:01:48 +0500

    Remove extra spaces
    Signed-off-by: Moeez Ahmed <x311099@gmail.com>

--- addOns/webuipoc/src/main/pocs/reactWebUI/src/index.js
+++ addOns/webuipoc/src/main/pocs/reactWebUI/src/index.js
@@ -3,12 +3,9 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
-
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
