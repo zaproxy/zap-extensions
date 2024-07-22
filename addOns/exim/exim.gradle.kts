08ae475bdca4cdff72729b0308b283d83658b132
kingthorin <kingthorin@users.noreply.github.com>
2024-07-22 12:37:30 -0400

    exim: Leverage Jackson from common-lib
    Signed-off-by: kingthorin <kingthorin@users.noreply.github.com>

--- addOns/exim/exim.gradle.kts
+++ addOns/exim/exim.gradle.kts
@@ -31,7 +31,7 @@ zapAddOn {
         dependencies {
             addOns {
                 register("commonlib") {
-                    version.set(">= 1.23.0 & < 2.0.0")
+                    version.set(">= 1.26.0 & < 2.0.0")
                 }
             }
         }
@@ -55,7 +55,10 @@ dependencies {
     zapAddOn("commonlib")
 
     implementation(files("lib/org.jwall.web.audit-0.2.15.jar"))
-    implementation("de.sstoehr:har-reader:2.3.0")
+    implementation("de.sstoehr:har-reader:2.3.0") {
+        // Provided by commonlib add-on:
+        exclude(group = "com.fasterxml.jackson.core")
+    }
 
     testImplementation(parent!!.childProjects.get("commonlib")!!.sourceSets.test.get().output)
     testImplementation(project(":testutils"))
