591d1460f809fa3134d1f365e4030546d8fa7abb
Moeez Ahmed <x311099@gmail.com>
2024-07-26 01:35:29 +0500

    Wired into CI
    Signed-off-by: Moeez Ahmed <x311099@gmail.com>

--- addOns/webuipoc/src/main/pocs/reactWebUI/src/Components/Accordion/AccordionData.jsx
+++ addOns/webuipoc/src/main/pocs/reactWebUI/src/Components/Accordion/AccordionData.jsx
@@ -1,57 +1,47 @@
 import React, { useState } from "react";
 
 const Accordion = ({ site, fetchChildren }) => {
-    const [isAccordionOpen, setAccordionOpen] = useState(false);
-    const [children, setChildren] = useState([])
+  const [isAccordionOpen, setAccordionOpen] = useState(false);
+  const [children, setChildren] = useState([]);
 
-    const handleExpand = async () => {
-        if (isAccordionOpen == false && site.isLeaf == false) {
-            const childNodes = await fetchChildren(site.name)
-            setChildren(childNodes)
-        }
-        setAccordionOpen(!isAccordionOpen);
+  const handleExpand = async () => {
+    if (isAccordionOpen == false && site.isLeaf == false) {
+      const childNodes = await fetchChildren(site.name);
+      setChildren(childNodes);
     }
-  
+    setAccordionOpen(!isAccordionOpen);
+  };
 
   return (
     <div className="py-1 w-[380px]">
-      <button
-        onClick={handleExpand}
-        className="flex justify-between w-full"
-      >
-        
+      <button onClick={handleExpand} className="flex justify-between w-full">
         <span className="pl-2">
-        {site.isLeaf? (
-          <span className="mr-3">-</span>
-        ) : (
-<> 
-      {!isAccordionOpen ? (
-          <span className="mr-3">▶</span>
-      ) : (
-        <span className="mr-3">▼</span>
-      )}
-    </>
-        )}
+          {site.isLeaf ? (
+            <span className="mr-3">-</span>
+          ) : (
+            <>
+              {!isAccordionOpen ? (
+                <span className="mr-3">▶</span>
+              ) : (
+                <span className="mr-3">▼</span>
+              )}
+            </>
+          )}
           <span className="" key={site}>
             {site.name}
           </span>
         </span>
       </button>
-     {
-              isAccordionOpen && (
-                  <div>
-                    <p className="text-blue-200 break-all">
-                      {
-                          children.map((child) => (
-                              <Accordion site={child} fetchChildren={fetchChildren}/>
-                          )) 
-                      }
-                      </p>
-                      </div>
-              )
-          }
-      </div>
-
+      {isAccordionOpen && (
+        <div>
+          <p className="text-blue-200 break-all">
+            {children.map((child) => (
+              <Accordion site={child} fetchChildren={fetchChildren} />
+            ))}
+          </p>
+        </div>
+      )}
+    </div>
   );
 };
 
