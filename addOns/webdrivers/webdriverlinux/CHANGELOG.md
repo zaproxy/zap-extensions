df0668ec836224d1b127559d1834048a29729bf8
zapbot <12745184+zapbot@users.noreply.github.com>
2024-09-24 23:05:46 +0000

    Webdriver Update
    ChromeDriver update to 129.0.6668.70
    
    Signed-off-by: zapbot <12745184+zapbot@users.noreply.github.com>

--- addOns/webdrivers/webdriverlinux/CHANGELOG.md
+++ addOns/webdrivers/webdriverlinux/CHANGELOG.md
@@ -4,7 +4,8 @@ All notable changes to this add-on will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/).
 
 ## Unreleased
-
+### Changed
+- Update ChromeDriver to 129.0.6668.70.
 
 ## [104] - 2024-09-18
 ### Changed
--- addOns/webdrivers/webdrivers.gradle.kts
+++ addOns/webdrivers/webdrivers.gradle.kts
@@ -6,7 +6,7 @@ import org.zaproxy.gradle.tasks.DownloadWebDriver
 description = "Common configuration of the WebDriver add-ons."
 
 val geckodriverVersion = "0.35.0"
-val chromeDriverVersion = "129.0.6668.58"
+val chromeDriverVersion = "129.0.6668.70"
 
 fun configureDownloadTask(
     outputDir: File,
