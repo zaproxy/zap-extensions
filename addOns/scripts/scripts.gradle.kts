0a32fe986c90124a891689cc733b9c08bae66014
kingthorin <kingthorin@users.noreply.github.com>
2024-11-28 11:18:25 -0500

    scripts: Add Policy Tags to ScriptsActiveScanner
    - CHANGELOG > Add notes.
    - scripts.gradle.kts > Newer commonlib dependency.
    - ScriptsActiveScanner > Override getAlertTags method.
    - ScriptsActiveScannerUnitTest> Add test for the new tags.
    
    Signed-off-by: kingthorin <kingthorin@users.noreply.github.com>

--- addOns/scripts/scripts.gradle.kts
+++ addOns/scripts/scripts.gradle.kts
@@ -30,7 +30,7 @@ zapAddOn {
         dependencies {
             addOns {
                 register("commonlib") {
-                    version.set(">=1.25.0")
+                    version.set(">=1.29.0")
                 }
             }
         }
