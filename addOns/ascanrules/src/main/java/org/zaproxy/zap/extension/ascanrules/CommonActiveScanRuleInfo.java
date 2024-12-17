0f25434ac9f24e34250f6db218d011f6415b9ab5
umair <umairbinyousuf786@gmail.com>
2024-12-17 14:16:55 +0500

    Link website alert pages

--- addOns/ascanrules/src/main/java/org/zaproxy/zap/extension/ascanrules/CommonActiveScanRuleInfo.java
+++ addOns/ascanrules/src/main/java/org/zaproxy/zap/extension/ascanrules/CommonActiveScanRuleInfo.java
@@ -29,7 +29,7 @@ public interface CommonActiveScanRuleInfo {
 
     default String getHelpLink(int id) {
 
-        Alert ID: <a href="https://www.zaproxy.org/docs/alerts/90020/">90020</a>.
+        Alert ID: <a href="https://www.zaproxy.org/docs/alerts/90020/">90020</a>
         return "https://www.zaproxy.org/docs/desktop/addons/active-scan-rules/#id-" + id;
     }
 }
