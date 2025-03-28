88b5266474fb29c544595fb42860b31cdf9ce3f5
Alice Milshtein <alicea.milshtein@gmail.com>
2025-03-28 00:34:20 -0400

    Add test web server with Totp Authentication that contains a captcha

--- addOns/dev/src/main/java/org/zaproxy/addon/dev/TestProxyServer.java
+++ addOns/dev/src/main/java/org/zaproxy/addon/dev/TestProxyServer.java
@@ -37,6 +37,7 @@ import org.zaproxy.addon.dev.api.openapi.simpleAuth.OpenApiSimpleAuthDir;
 import org.zaproxy.addon.dev.api.openapi.simpleAuthWithOTP.OpenApiWithOtpSimpleAuthDir;
 import org.zaproxy.addon.dev.api.openapi.simpleAuthTotpBlankCodeVuln.OpenApiWithBlankOtpSimpleAuthDir;
 import org.zaproxy.addon.dev.api.openapi.simpleAuthTotpLockout.OpenApiWithLockoutOtpSimpleAuthDir;
+import org.zaproxy.addon.dev.api.openapi.simpleAuthTotpCaptcha.OpenApiWithCaptchaOtpSimpleAuthDir;
 import org.zaproxy.addon.dev.api.openapi.simpleUnauth.OpenApiSimpleUnauthDir;
 import org.zaproxy.addon.dev.auth.jsonMultipleCookies.JsonMultipleCookiesDir;
 import org.zaproxy.addon.dev.auth.nonStdJsonBearer.NonStdJsonBearerDir;
@@ -97,6 +98,7 @@ public class TestProxyServer {
         openapiDir.addDirectory(new OpenApiWithOtpSimpleAuthDir(this, "simple-auth-with-otp"));
         openapiDir.addDirectory(new OpenApiWithBlankOtpSimpleAuthDir(this, "simple-auth-otp-blank-code-vuln"));
         openapiDir.addDirectory(new OpenApiWithLockoutOtpSimpleAuthDir(this, "simple-auth-otp-lockout"));
+        openapiDir.addDirectory(new OpenApiWithCaptchaOtpSimpleAuthDir(this, "simple-auth-otp-captcha"));
         openapiDir.addDirectory(new OpenApiSimpleUnauthDir(this, "simple-unauth"));
 
         TestDirectory htmlDir = new TestDirectory(this, "html");
---
+++ addOns/dev/src/main/java/org/zaproxy/addon/dev/api/openapi/simpleAuthTotpCaptcha/OpenApiWithCaptchaOtpVerificationPage.java
@@ -0,0 +1,89 @@
+/*
+ * Zed Attack Proxy (ZAP) and its related class files.
+ *
+ * ZAP is an HTTP/HTTPS proxy for assessing web application security.
+ *
+ * Copyright 2023 The ZAP Development Team
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.zaproxy.addon.dev.api.openapi.simpleAuthTotpCaptcha;
+
+import net.sf.json.JSONException;
+import net.sf.json.JSONObject;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.parosproxy.paros.network.HttpHeader;
+import org.parosproxy.paros.network.HttpMessage;
+import org.zaproxy.addon.dev.TestPage;
+import org.zaproxy.addon.dev.TestProxyServer;
+import org.zaproxy.addon.network.server.HttpMessageHandlerContext;
+
+import java.util.concurrent.ConcurrentHashMap;
+
+public class OpenApiWithCaptchaOtpVerificationPage extends TestPage {
+
+    private static final Logger LOGGER = LogManager.getLogger(OpenApiWithCaptchaOtpVerificationPage.class);
+    private static final ConcurrentHashMap<String, String> captchaStore = new ConcurrentHashMap<>();
+
+    public OpenApiWithCaptchaOtpVerificationPage(TestProxyServer server) {
+        super(server, "user");
+    }
+
+    @Override
+    public void handleMessage(HttpMessageHandlerContext ctx, HttpMessage msg) {
+        String totp = null;
+        String captcha = null;
+
+        if (msg.getRequestHeader().hasContentType("json")) {
+            String postData = msg.getRequestBody().toString();
+            JSONObject jsonObject;
+            try {
+                jsonObject = JSONObject.fromObject(postData);
+                totp = jsonObject.getString("code");
+                captcha = jsonObject.getString("captcha");
+            } catch (JSONException e) {
+                LOGGER.debug("Unable to parse as JSON: {}", postData, e);
+            }
+        }
+
+        String token = msg.getRequestHeader().getHeader(HttpHeader.AUTHORIZATION);
+        String user = getParent().getUser(token);
+
+        // Validate CAPTCHA
+        String expectedCaptcha = captchaStore.remove(token);
+        boolean isCaptchaValid = expectedCaptcha != null && expectedCaptcha.equals(captcha);
+
+        LOGGER.debug("Token: {} user: {} TOTP: {} CAPTCHA: {} Expected CAPTCHA: {}", token, user, totp, captcha, expectedCaptcha);
+
+        JSONObject response = new JSONObject();
+        if (user != null && totp != null && totp.equals("123456") && isCaptchaValid) {
+            response.put("result", "OK");
+            response.put("user", user);
+        } else {
+            response.put("result", "FAIL");
+        }
+
+        // Generate a new CAPTCHA for the next request
+        String newCaptcha = "captcha" + (int) (Math.random() * 10000);
+        captchaStore.put(token, newCaptcha);
+        response.put("newCaptcha", newCaptcha);
+
+        this.getServer().setJsonResponse(response, msg);
+    }
+
+    @Override
+    public OpenApiWithCaptchaOtpSimpleAuthDir getParent() {
+        return (OpenApiWithCaptchaOtpSimpleAuthDir) super.getParent();
+    }
+}
