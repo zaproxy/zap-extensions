7933c5873519efeb3f4c81a33bd2693f778a3509
kingthorin <kingthorin@users.noreply.github.com>
2025-09-11 16:51:16 -0400

    pscanrulesBeta: Add valid reference a check unit tests
    Signed-off-by: kingthorin <kingthorin@users.noreply.github.com>

--- addOns/pscanrulesBeta/src/main/java/org/zaproxy/zap/extension/pscanrulesBeta/SiteIsolationScanRule.java
+++ addOns/pscanrulesBeta/src/main/java/org/zaproxy/zap/extension/pscanrulesBeta/SiteIsolationScanRule.java
@@ -54,13 +54,13 @@ import org.zaproxy.zap.extension.pscan.PluginPassiveScanner;
  * </ul>
  *
  * @see <a
- *     href="https://developer.mozilla.org/en-US/docs/Web/HTTP/Cross-Origin_Resource_Policy_(CORP)">CORP
+ *     href="https://developer.mozilla.org/en-US/docs/Web/HTTP/Guides/Cross-Origin_Resource_Policy">CORP
  *     on MDN</a>
  * @see <a
- *     href="https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Cross-Origin-Embedder-Policy">COEP
+ *     href="https://developer.mozilla.org/en-US/docs/Web/HTTP/Reference/Headers/Cross-Origin-Embedder-Policy">COEP
  *     on MDN</a>
  * @see <a
- *     href="https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Cross-Origin-Opener-Policy">COOP
+ *     href="https://developer.mozilla.org/en-US/docs/Web/HTTP/Reference/Headers/Cross-Origin-Opener-Policy">COOP
  *     on MDN</a>
  * @see <a href="https://fetch.spec.whatwg.org/#cross-origin-resource-policy-header">COOP Specs</a>
  * @see <a href="https://html.spec.whatwg.org/multipage/origin.html#coep">COEP Specs</a>
