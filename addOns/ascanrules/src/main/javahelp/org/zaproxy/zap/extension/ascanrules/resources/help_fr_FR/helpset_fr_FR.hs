1aec3c85caf80914c98aabfb75d4baacf454082e
zapbot <12745184+zapbot@users.noreply.github.com>
2026-04-27 07:27:40 +0000

    Update localized resources
    Update resources from Crowdin.
    
    Signed-off-by: zapbot <12745184+zapbot@users.noreply.github.com>

--- addOns/ascanrules/src/main/javahelp/org/zaproxy/zap/extension/ascanrules/resources/help_fr_FR/helpset_fr_FR.hs
+++ addOns/ascanrules/src/main/javahelp/org/zaproxy/zap/extension/ascanrules/resources/help_fr_FR/helpset_fr_FR.hs
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="UTF-8"?><!DOCTYPE helpset PUBLIC "-//Sun Microsystems Inc.//DTD JavaHelp HelpSet Version 2.0//EN" "http://java.sun.com/products/javahelp/helpset_2_0.dtd">
 <helpset version="2.0" xml:lang="fr-FR">
-  <title>Active Scan Rules | ZAP Extension</title>
+  <title>Règles d’analyse active | Extension ZAP</title>
 
   <maps>
      <homeID>top</homeID>
@@ -9,14 +9,14 @@
 
   <view>
     <name>TOC</name>
-    <label>Contents</label>
+    <label>Contenus</label>
     <type>org.zaproxy.zap.extension.help.ZapTocView</type>
     <data>toc.xml</data>
   </view>
 
   <view>
     <name>Index</name>
-    <label>Indice</label>
+    <label>Index</label>
     <type>javax.help.IndexView</type>
     <data>index.xml</data>
   </view>
@@ -32,7 +32,7 @@
 
   <view>
     <name>Favorites</name>
-    <label>Favorites</label>
+    <label>Favoris</label>
     <type>javax.help.FavoritesView</type>
   </view>
 </helpset>
